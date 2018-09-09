3d8e59780bd163d2db8e851bc9801c6d236a0b9e
Marcel Cohen <diegomarcel27@hotmail.com>
2018-09-09 19:39:59 -0300

    se agrega clase de prueba para la entidad User

---
+++ Arancet_Cohen/ObligatorioDA2/BusinessLogicTest/UserTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using BusinessLogicTest;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BusinessLogicTest
+{
+    [TestClass]
+    public class UserTest
+    {
+        User toTest;
+
+        [TestInitialize]
+        public void SetUp() {
+            toTest = new Admin("name", "surname", "username", "password", "email");
+        }
+        [TestMethod]
+        public void GetNameTest()
+        {
+            Assert.AreEqual("name", toTest.Name);
+        }
+
+        
+    }
+}
