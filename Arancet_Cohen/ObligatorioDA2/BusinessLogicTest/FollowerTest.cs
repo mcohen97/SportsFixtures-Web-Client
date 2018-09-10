28bb2c0e9894c6e932359f8e69c3e2f39d05bd23
Marcel Cohen <diegomarcel27@hotmail.com>
2018-09-10 20:11:50 -0300

    se agrega metodo para testear la propiedad IsAdmin

--- Arancet_Cohen/ObligatorioDA2/BusinessLogicTest/FollowerTest.cs
+++ Arancet_Cohen/ObligatorioDA2/BusinessLogicTest/FollowerTest.cs
@@ -1,11 +1,18 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BusinessLogic;
 
 namespace BusinessLogicTest
 {
     [TestClass]
     public class FollowerTest
     {
-        
+        [TestMethod]
+        public void IsAdmintest()
+        {
+            User test = new Follower("name", "surname", "username", "password", "mail@domain.com");
+            Assert.IsFalse(test.IsAdmin);
+        }
+
     }
 }
