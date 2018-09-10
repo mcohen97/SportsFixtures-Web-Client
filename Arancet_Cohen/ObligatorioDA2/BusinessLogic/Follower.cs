73c4a7c31a2ab6e204442f6a597b8a7053a279a6
Marcel Cohen <diegomarcel27@hotmail.com>
2018-09-10 20:23:11 -0300

    se agrega la clase follower y se ewcribe el constructor para que pase la prueba

---
+++ Arancet_Cohen/ObligatorioDA2/BusinessLogic/Follower.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public class Follower : User
+    {
+        public override bool IsAdmin { get; set; }
+        public Follower(string aName, string aSurname, string aUserName, string aPassword, string anEmail) : base(aName, aSurname, aUserName, aPassword, anEmail)
+        {
+
+        }
+    }
+}
