b18e051b0a6765dbbf0a9b2370b4d913ff718b49
Marcel Cohen <diegomarcel27@hotmail.com>
2018-09-10 19:48:19 -0300

    se agregan pruebas para Admin

---
+++ Arancet_Cohen/ObligatorioDA2/BusinessLogicTest/AdminTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BusinessLogic;
+using Moq;
+
+namespace BusinessLogicTest
+{
+    [TestClass]
+    public class AdminTest
+    {
+        [TestMethod]
+        public void IsAdminTest()
+        {
+            Mock<User> fake = new Mock<User>("name", "surname", "username", "password", "mail@domain.com");
+            Assert.IsTrue(fake.Object.IsAdmin);
+        }
+    }
+}
