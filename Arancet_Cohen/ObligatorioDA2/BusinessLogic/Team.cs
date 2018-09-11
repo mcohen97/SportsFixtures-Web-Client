a3fa7f68168a8fd8354250fe9beba6fec5f16106
Facundo Arancet Iza <facuarancet97@gmail.com>
2018-09-11 19:31:01 -0300

    Pruebas de Team verdes

--- Arancet_Cohen/ObligatorioDA2/BusinessLogic/Team.cs
+++ Arancet_Cohen/ObligatorioDA2/BusinessLogic/Team.cs
@@ -2,5 +2,39 @@
 {
     public class Team
     {
+        private string name;
+        private string photo;
+
+        public Team()
+        {
+            name = "";
+            photo = "";
+        }
+        public Team(string name, string photo)
+        {
+            this.name = name;
+            this.photo = photo;
+        }
+
+        public string Name {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public string Photo {
+            get
+            {
+                return photo;
+            }
+            set
+            {
+                photo = value;
+            }
+        }
     }
 }
--- Arancet_Cohen/ObligatorioDA2/BusinessLogicTest/TeamTest.cs
+++ Arancet_Cohen/ObligatorioDA2/BusinessLogicTest/TeamTest.cs
@@ -17,8 +17,46 @@ namespace BusinessLogicTest
         [TestMethod]
         public void ConstructorParametersTeamTest()
         {
-            Team team = new Team();
+            string name = "TheTeam";
+            string photo = "myresource/theteam.png";
+            Team team = new Team(name, photo);
             Assert.IsNotNull(team);
+        }
+
+        [TestMethod]
+        public void GetNameTest()
+        {
+            string name = "TheTeam";
+            string photo = "myresourcce/theteam.png";
+            Team team = new Team(name, photo);
+            Assert.AreEqual(name, team.Name);
+        }
+
+        [TestMethod]
+        public void SetNameTest()
+        {
+            string name = "TheTeam";
+            Team team = new Team();
+            team.Name = name;
+            Assert.AreEqual(name, team.Name);
+        }
+
+        [TestMethod]
+        public void GetPhotoTest()
+        {
+            string name = "TheTeam";
+            string photo = "myresource/theteam.png";
+            Team team = new Team(name, photo);
+            Assert.AreEqual(photo, team.Photo);
+        }
+
+        [TestMethod]
+        public void SetPhotoTest()
+        {
+            string photo = "myresource/theteam.png";
+            Team team = new Team();
+            team.Photo = photo;
+            Assert.AreEqual(photo, team.Photo);
         }
     }
 }
