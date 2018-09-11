03ac5399370c5aa81d1088da4714dccb43ea1612
Marcel Cohen <diegomarcel27@hotmail.com>
2018-09-11 15:35:33 -0300

    se agrega la clase contexto a DataAccess

---
+++ Arancet_Cohen/ObligatorioDA2/DataAccess/DatabaseConnection.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess
+{
+    class DatabaseConnection:DbContext
+    {
+        public virtual DbContext Users { get; set; }
+        public DatabaseConnection(DbContextOptions<DatabaseConnection> options) : base(options)
+        {
+        }
+
+    }
+}
