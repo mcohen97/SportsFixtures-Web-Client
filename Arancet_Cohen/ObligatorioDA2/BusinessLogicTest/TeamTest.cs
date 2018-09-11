bc98032cea01e9658cb4b4c35e8c8a9b5ab5b37d
Facundo Arancet Iza <facuarancet97@gmail.com>
2018-09-11 19:26:53 -0300

    Pruebas de Team agregadas

---
+++ Arancet_Cohen/ObligatorioDA2/BusinessLogicTest/TeamTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BusinessLogic;
+
+namespace BusinessLogicTest
+{
+    [TestClass]
+    public class TeamTest
+    {
+        [TestMethod]
+        public void ConstructorTeamTest()
+        {
+            Team team = new Team();
+            Assert.IsNotNull(team);
+        }
+
+        [TestMethod]
+        public void ConstructorParametersTeamTest()
+        {
+            Team team = new Team();
+            Assert.IsNotNull(team);
+        }
+    }
+}
