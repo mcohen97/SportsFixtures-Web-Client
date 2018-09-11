b117db6f73ba67ce71f10c4687e4a2352b472734
Marcel Cohen <diegomarcel27@hotmail.com>
2018-09-11 16:44:09 -0300

    Se renombra Repository Interface, se agregan proyectos DataRepositoryInterface, DataRepositories y DataRepositoriesTest. Tambien se agrega clase de prueba para repositorio de users

--- Arancet_Cohen/ObligatorioDA2/DataAccess/DatabaseConnection.cs
+++ Arancet_Cohen/ObligatorioDA2/DataAccess/DatabaseConnection.cs
@@ -5,12 +5,11 @@ using System.Text;
 
 namespace DataAccess
 {
-    class DatabaseConnection:DbContext
+    public class DatabaseConnection:DbContext
     {
         public virtual DbContext Users { get; set; }
         public DatabaseConnection(DbContextOptions<DatabaseConnection> options) : base(options)
         {
         }
-
     }
 }
---
+++ Arancet_Cohen/ObligatorioDA2/DataRepositoriesTest/UserRepositoryTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using DataAccess;
+
+namespace DataAccessTest
+{
+    [TestClass]
+    public class UserRepositoryTest
+    {
+        IUserRepository usersStorage;
+
+        [TestInitialize]
+        public void SetUp() {
+            Mock<DatabaseConnection> connection = new Mock<DatabaseConnection>();
+            usersStorage = new UserRepository();
+        }
+        [TestMethod]
+        public void GetUserTest()
+        {
+
+        }
+    }
+}
