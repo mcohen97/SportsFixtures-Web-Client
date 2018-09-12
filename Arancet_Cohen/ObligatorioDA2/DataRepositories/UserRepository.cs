23966044b60dec0746112c155bedac133943dea4
Marcel Cohen <diegomarcel27@hotmail.com>
2018-09-11 23:35:58 -0300

    se implementa la funcion con la minima implementacion para que las pruebas compilen

--- Arancet_Cohen/ObligatorioDA2/DataRepositories/UserRepository.cs
+++ Arancet_Cohen/ObligatorioDA2/DataRepositories/UserRepository.cs
@@ -2,10 +2,22 @@
 using System.Collections.Generic;
 using System.Text;
 using DataRepositoryInterfaces;
+using DataAccess;
+using BusinessLogic;
 
 namespace DataRepositories
 {
-    public class UserRepository:IUserRepository
+    public class UserRepository : IUserRepository
     {
+        private readonly ContextFactory creator;
+        public UserRepository(ContextFactory aFactory)
+        {
+           creator = aFactory;
+        }
+
+        public User GetUserByUsername(string aUsername)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Arancet_Cohen/ObligatorioDA2/DataRepositoryInterfaces/IUserRepository.cs
+++ Arancet_Cohen/ObligatorioDA2/DataRepositoryInterfaces/IUserRepository.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using BusinessLogic;
 
 namespace DataRepositoryInterfaces
 {
     public interface IUserRepository
     {
+        User GetUserByUsername(string aUsername);
     }
 }
