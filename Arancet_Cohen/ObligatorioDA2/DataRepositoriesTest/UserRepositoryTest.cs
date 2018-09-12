129055d0c1d56634d08deeae64940ddd0c7bd95f
Marcel Cohen <diegomarcel27@hotmail.com>
2018-09-11 23:24:48 -0300

    se escribe prueba para metodo GetByUsername

--- Arancet_Cohen/ObligatorioDA2/DataRepositoriesTest/UserRepositoryTest.cs
+++ Arancet_Cohen/ObligatorioDA2/DataRepositoriesTest/UserRepositoryTest.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
 using Moq;
 using DataAccess;
 using DataRepositoryInterfaces;
 using DataRepositories;
+using System.Collections.Generic;
+using BusinessLogic;
+using Microsoft.EntityFrameworkCore;
 
 namespace DataAccessTest
 {
@@ -14,14 +18,33 @@ namespace DataAccessTest
 
         [TestInitialize]
         public void SetUp() {
-            Mock<DatabaseConnection> connection = new Mock<DatabaseConnection>();
-           
-            //usersStorage = new UserRepository(connec);
+            Mock<User> user1 = new Mock<User>();
+            Mock<User> user2 = new Mock<User>();
+            Mock<User> user3 = new Mock<User>();
+
+            user1.Setup(u => u.UserName).Returns("user1");
+            user1.Setup(u => u.Name).Returns("name1");
+            user2.Setup(u => u.UserName).Returns("user2");
+            user3.Setup(u => u.UserName).Returns("user3");
+
+            ICollection<Mock<User>> users = new List<Mock<User>>() {user1,user2,user3};
+            Mock<DbSet<User>> mockSet = new Mock<DbSet<User>>();
+            mockSet.Object.AddRange(users.Select(m=>m.Object));
+            
+
+            Mock<DatabaseConnection> fakeDB = new Mock<DatabaseConnection>();
+            fakeDB.Setup(db => db.Users).Returns(mockSet.Object);
+
+            ContextFactory factory = new ContextFactory();
+            factory.Register<DatabaseConnection>(fakeDB.Object);
+                        
+            usersStorage = new UserRepository(factory);
         }
         [TestMethod]
         public void GetUserTest()
         {
-
+            User queried = usersStorage.GetUserByUsername("user1");
+            Assert.AreEqual("name1", queried.Name);
         }
     }
 }
