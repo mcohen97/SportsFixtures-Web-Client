8e634cad31368c3d79546dea19a4262bb92d40c8
Marcel Cohen <diegomarcel27@hotmail.com>
2018-09-11 23:21:22 -0300

    se corrije atributo de la clase contexto

--- Arancet_Cohen/ObligatorioDA2/DataAccess/DatabaseConnection.cs
+++ Arancet_Cohen/ObligatorioDA2/DataAccess/DatabaseConnection.cs
@@ -2,12 +2,13 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using BusinessLogic;
 
 namespace DataAccess
 {
     public class DatabaseConnection:DbContext
     {
-        public virtual DbContext Users { get; set; }
+        public virtual DbSet<User> Users { get; set; }
         public DatabaseConnection(DbContextOptions<DatabaseConnection> options) : base(options)
         {
         }
