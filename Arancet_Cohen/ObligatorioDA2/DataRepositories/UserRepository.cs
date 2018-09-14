17d5a30fdc3d184da5278d927d84253bb27bf4d7
Marcel Cohen <diegomarcel27@hotmail.com>
2018-09-14 18:17:32 -0300

    se implementan las funcionens probadas

--- Arancet_Cohen/ObligatorioDA2/DataRepositories/UserRepository.cs
+++ Arancet_Cohen/ObligatorioDA2/DataRepositories/UserRepository.cs
@@ -5,10 +5,11 @@ using System.Linq;
 using DataRepositoryInterfaces;
 using DataAccess;
 using BusinessLogic;
+using RepositoryInterface;
 
 namespace DataRepositories
 {
-    public class UserRepository : IUserRepository
+    public class UserRepository : IUserRepository, IRepository<User>
     {
         private readonly ContextFactory creator;
         public UserRepository(ContextFactory aFactory)
@@ -16,13 +17,65 @@ namespace DataRepositories
            creator = aFactory;
         }
 
+        public void Add(User entity)
+        {
+            using (DatabaseConnection dbConn = creator.Get()) {
+                dbConn.Users.Add(entity);
+                dbConn.SaveChanges();
+            }
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(User entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Exists(User record)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Get(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Get(User asked)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<User> GetAll()
+        {
+            ICollection<User> query;
+            using (DatabaseConnection dbConn = creator.Get()) {
+                query = dbConn.Users.ToList();
+            }
+            return query;
+        }
+
         public User GetUserByUsername(string aUsername)
         {
             User toReturn;
-            using (DatabaseConnection db = creator.Get<DatabaseConnection>()) {
+            using (DatabaseConnection db = creator.Get()) {
                     toReturn = db.Users.First(u => u.UserName.Equals(aUsername));              
             }
             return toReturn;
+        }
+
+        public bool IsEmpty()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Modify(User entity)
+        {
+            throw new NotImplementedException();
         }
     }
 }
