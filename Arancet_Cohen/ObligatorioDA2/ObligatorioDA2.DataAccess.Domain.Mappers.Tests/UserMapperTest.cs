4900b23b4c057f11a1efd399752c198fef434d57
Marcel Cohen <diegomarcel27@hotmail.com>
2018-09-16 16:36:38 -0300

    se escriben pruebas para el metodo de conversion del mapper de Elemento de User a UserEntity

--- Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.DataAccess.Domain.Mappers.Tests/UserMapperTest.cs
+++ Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.DataAccess.Domain.Mappers.Tests/UserMapperTest.cs
@@ -1,13 +1,37 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using BusinessLogic;
+using ObligatorioDA2.DataAccess.Entities;
 
 namespace ObligatorioDA2.DataAccess.Domain.Mappers.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class UserMapperTest
     {
+        Mock<User> toConvert;
+        UserMapper toTest;
+
+        [TestInitialize]
+        public void StartUp() {
+            toConvert = new Mock<User>("name", "surname", "username", "password", "email@domain.com");
+            toTest = new UserMapper();
+        }
         [TestMethod]
-        public void TestMethod1()
+        public void UserToEntityNameTest()
         {
+            UserEntity conversion = toTest.ToEntity(toConvert.Object);
+            Assert.AreEqual(conversion.Name, toConvert.Object.Name);
+        }
+
+        public void UserToEntitySurnameTest()
+        {
+            UserEntity conversion = toTest.ToEntity(toConvert.Object);
+            Assert.AreEqual(conversion.Surname, toConvert.Object.Surname);
+        }
+        public void UserToEntityUserNameTest()
+        {
+            UserEntity conversion = toTest.ToEntity(toConvert.Object);
+            Assert.AreEqual(conversion.Surname, toConvert.Object.Surname);
         }
     }
 }
