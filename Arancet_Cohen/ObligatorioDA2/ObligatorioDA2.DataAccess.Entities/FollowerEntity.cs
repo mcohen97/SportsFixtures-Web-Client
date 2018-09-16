396fb7abab1a569096a6ac881939df02b3a7a93d
Marcel Cohen <diegomarcel27@hotmail.com>
2018-09-16 00:23:56 -0300

    se agregan entidades para Follower y Admin

---
+++ Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.DataAccess.Entities/FollowerEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObligatorioDA2.DataAccess.Entities
+{
+    class FollowerEntity:UserEntity
+    {
+        public FollowerEntity() {
+            IsAdmin = false;
+        }
+    }
+}
--- Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.DataAccess.Entities/UserEntity.cs
+++ Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.DataAccess.Entities/UserEntity.cs
@@ -12,5 +12,7 @@ namespace ObligatorioDA2.DataAccess.Entities
         public string UserName { get; private set; }
 
         public string Email { get; private set; }
+
+        public bool IsAdmin { get; protected set; }
     }
 }
