e9920d47d2877dbf0d9058661fbafcea85a7b6ed
Marcel Cohen <diegomarcel27@hotmail.com>
2018-09-16 13:58:43 -0300

    se crea proyecto de pruebas para userService

---
+++ Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.Services.Tests/UserServiceTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ObligatorioDA2.Services.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
