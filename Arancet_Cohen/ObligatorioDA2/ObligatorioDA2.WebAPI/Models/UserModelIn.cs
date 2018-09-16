b0049b818f31036ec293006a871bb5324a52aa6a
Marcel Cohen <diegomarcel27@hotmail.com>
2018-09-16 13:47:58 -0300

    se agregan los atributos de campos requeridos en UserModelIn para que las pruebas pasen

--- Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.WebAPI/Models/UserModelIn.cs
+++ Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.WebAPI/Models/UserModelIn.cs
@@ -1,16 +1,21 @@
-﻿namespace ObligatorioDA2.WebAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ObligatorioDA2.WebAPI.Models
 {
     public class UserModelIn
     {
         public UserModelIn()
         {
         }
-
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Surname { get; set; }
+        [Required]
         public string Username { get; set; }
+        [Required]
         public string Password { get; set; }
-
+        [Required]
         public string Email { get; set; }
     }
 }
