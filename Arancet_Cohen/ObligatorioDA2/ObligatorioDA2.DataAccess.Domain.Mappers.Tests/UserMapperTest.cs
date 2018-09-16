34bcc10d544a09ea4d1138cfcb6b84de43348f4f
Marcel Cohen <diegomarcel27@hotmail.com>
2018-09-16 16:16:46 -0300

    se agrega proyecto de mappers de dominio a entidades  y uno para probarlo

---
+++ Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.DataAccess.Domain.Mappers.Tests/UserMapperTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ObligatorioDA2.DataAccess.Domain.Mappers.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
