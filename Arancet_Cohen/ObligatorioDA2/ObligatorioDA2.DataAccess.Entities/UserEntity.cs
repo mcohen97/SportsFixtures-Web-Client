68c10db852d1f636525dc759abf4e9ff3333213b
Marcel Cohen <diegomarcel27@hotmail.com>
2018-09-16 15:28:19 -0300

    se agrega property olvidada a UserEntity

--- Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.DataAccess.Entities/UserEntity.cs
+++ Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.DataAccess.Entities/UserEntity.cs
@@ -11,6 +11,8 @@ namespace ObligatorioDA2.DataAccess.Entities
 
         public string UserName { get; private set; }
 
+        public string Password { get; private set; }
+
         public string Email { get; private set; }
 
         public bool IsAdmin { get; protected set; }
