a9b60867bed66e8754b0a54ecf190bbf5954c598
Marcel Cohen <diegomarcel27@hotmail.com>
2018-09-15 22:46:08 -0300

    se modifica UserModelOut

--- Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.WebAPI/Models/UserModelOut.cs
+++ Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.WebAPI/Models/UserModelOut.cs
@@ -8,8 +8,12 @@ namespace ObligatorioDA2.WebAPI.Models
     public class UserModelOut
     {
         public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
         public string Username { get; set; }
-        public int Age { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+
 
     }
 }
