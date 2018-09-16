257ee7617b70900bb37b407bcf1f0ee8ae555032
Marcel Cohen <diegomarcel27@hotmail.com>
2018-09-16 13:47:16 -0300

    se agregan pruebas para campos requeridos en UserModelIn

--- Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.WebAPI.Tests/UserControllerTest.cs
+++ Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.WebAPI.Tests/UserControllerTest.cs
@@ -28,10 +28,15 @@ namespace ObligatorioDA2.WebAPI.Tests
         }
 
         [TestMethod]
-        public void CreateFailedUserTest()
+        public void CreateFailedUserRequiredMailTest()
         {
             //Arrange
-            var modelIn = new UserModelIn();
+            var modelIn = new UserModelIn() {
+                Name= "name",
+                Surname="surname",
+                Username ="username",
+                Password="password"
+            };
             var controller = new UsersController();
             //We need to force the error in de ModelState
             controller.ModelState.AddModelError("", "Error");
@@ -42,5 +47,94 @@ namespace ObligatorioDA2.WebAPI.Tests
             Assert.IsNotNull(createdResult);
             Assert.AreEqual(400, createdResult.StatusCode);
         }
+
+        [TestMethod]
+        public void CreateFailedUserRequiredPasswordTest()
+        {
+            //Arrange
+            var modelIn = new UserModelIn()
+            {
+                Name = "name",
+                Surname = "surname",
+                Username = "username",
+                Email = "email"
+            };
+            var controller = new UsersController();
+            //We need to force the error in de ModelState
+            controller.ModelState.AddModelError("", "Error");
+            var result = controller.Post(modelIn);
+            //Act
+            var createdResult = result as BadRequestObjectResult;
+            //Assert
+            Assert.IsNotNull(createdResult);
+            Assert.AreEqual(400, createdResult.StatusCode);
+        }
+
+        [TestMethod]
+        public void CreateFailedUserRequiredUsernameTest()
+        {
+            //Arrange
+            var modelIn = new UserModelIn()
+            {
+                Name = "name",
+                Surname = "surname",
+                Password = "password",
+                Email = "email"
+            };
+            var controller = new UsersController();
+            //We need to force the error in de ModelState
+            controller.ModelState.AddModelError("", "Error");
+            var result = controller.Post(modelIn);
+            //Act
+            var createdResult = result as BadRequestObjectResult;
+            //Assert
+            Assert.IsNotNull(createdResult);
+            Assert.AreEqual(400, createdResult.StatusCode);
+        }
+
+        [TestMethod]
+        public void CreateFailedUserRequiredNameTest()
+        {
+            //Arrange
+            var modelIn = new UserModelIn()
+            {
+                Surname = "surname",
+                Username= "username",
+                Password = "password",
+                Email = "email"
+            };
+            var controller = new UsersController();
+            //We need to force the error in de ModelState
+            controller.ModelState.AddModelError("", "Error");
+            var result = controller.Post(modelIn);
+            //Act
+            var createdResult = result as BadRequestObjectResult;
+            //Assert
+            Assert.IsNotNull(createdResult);
+            Assert.AreEqual(400, createdResult.StatusCode);
+        }
+
+        [TestMethod]
+        public void CreateFailedUserRequiredSurnameTest()
+        {
+            //Arrange
+            var modelIn = new UserModelIn()
+            {
+                Name = "name",
+                Username = "username",
+                Password = "password",
+                Email = "email"
+            };
+            var controller = new UsersController();
+            //We need to force the error in de ModelState
+            controller.ModelState.AddModelError("", "Error");
+            var result = controller.Post(modelIn);
+            //Act
+            var createdResult = result as BadRequestObjectResult;
+            //Assert
+            Assert.IsNotNull(createdResult);
+            Assert.AreEqual(400, createdResult.StatusCode);
+        }
+
     }
 }
