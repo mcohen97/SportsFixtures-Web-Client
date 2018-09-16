c97430b61c20f76ac2dc31b91f440bac14c42a36
Marcel Cohen <diegomarcel27@hotmail.com>
2018-09-16 00:14:44 -0300

    se crea proyecto de Entities de almacenamiento con una entity de usuario

---
+++ Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.DataAccess.Entities/UserEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ObligatorioDA2.DataAccess.Entities
+{
+    public class UserEntity
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+
+        public string Surname { get; private set; }
+
+        public string UserName { get; private set; }
+
+        public string Email { get; private set; }
+    }
+}
