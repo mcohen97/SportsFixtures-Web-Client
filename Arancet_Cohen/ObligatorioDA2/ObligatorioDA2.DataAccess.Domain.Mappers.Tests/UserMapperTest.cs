690b8ab7c9f2aca81b536127f11415c03a7c4769
Marcel Cohen <diegomarcel27@hotmail.com>
2018-09-16 17:12:49 -0300

    se agregan pruebas para el metodo de Mapper que convierte entidades en objetos de dominio

--- Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.DataAccess.Domain.Mappers.Tests/UserMapperTest.cs
+++ Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.DataAccess.Domain.Mappers.Tests/UserMapperTest.cs
@@ -9,30 +9,91 @@ namespace ObligatorioDA2.DataAccess.Domain.Mappers.Tests
     [TestClass]
     public class UserMapperTest
     {
-        Mock<User> toConvert;
+        Mock<User> toStore;
+        UserEntity toGet;
         UserMapper toTest;
 
         [TestInitialize]
         public void StartUp() {
-            toConvert = new Mock<User>("name", "surname", "username", "password", "email@domain.com");
+            toStore = new Mock<User>("name", "surname", "username", "password", "email@domain.com");
+            toGet = new UserEntity()
+            {
+                Name = "name",
+                Surname = "surname",
+                UserName = "username",
+                Password = "password",
+                Email = "email@domain.com"
+            };
             toTest = new UserMapper();
         }
+
         [TestMethod]
         public void UserToEntityNameTest()
         {
-            UserEntity conversion = toTest.ToEntity(toConvert.Object);
-            Assert.AreEqual(conversion.Name, toConvert.Object.Name);
+            UserEntity conversion = toTest.ToEntity(toStore.Object);
+            Assert.AreEqual(conversion.Name, toStore.Object.Name);
         }
 
+        [TestMethod]
         public void UserToEntitySurnameTest()
         {
-            UserEntity conversion = toTest.ToEntity(toConvert.Object);
-            Assert.AreEqual(conversion.Surname, toConvert.Object.Surname);
+            UserEntity conversion = toTest.ToEntity(toStore.Object);
+            Assert.AreEqual(conversion.Surname, toStore.Object.Surname);
         }
+
+        [TestMethod]
         public void UserToEntityUserNameTest()
         {
-            UserEntity conversion = toTest.ToEntity(toConvert.Object);
-            Assert.AreEqual(conversion.Surname, toConvert.Object.Surname);
+            UserEntity conversion = toTest.ToEntity(toStore.Object);
+            Assert.AreEqual(conversion.UserName, toStore.Object.UserName);
+        }
+
+        [TestMethod]
+        public void UserToEntityPasswordTest()
+        {
+            UserEntity conversion = toTest.ToEntity(toStore.Object);
+            Assert.AreEqual(conversion.Password, toStore.Object.Password);
+        }
+
+        [TestMethod]
+        public void UserToEntityEmailTest()
+        {
+            UserEntity conversion = toTest.ToEntity(toStore.Object);
+            Assert.AreEqual(conversion.Email, toStore.Object.Email);
+        }
+
+        [TestMethod]
+        public void EntityToUserNameTest() {
+            User conversion = toTest.ToUser(toGet);
+            Assert.AreEqual(conversion.Name, toGet.Name);
+        }
+
+        [TestMethod]
+        public void EntityToUserSurnameTest()
+        {
+            User conversion = toTest.ToUser(toGet);
+            Assert.AreEqual(conversion.Surname, toGet.Surname);
+        }
+
+        [TestMethod]
+        public void EntityToUserUserNameTest()
+        {
+            User conversion = toTest.ToUser(toGet);
+            Assert.AreEqual(conversion.UserName, toGet.UserName);
+        }
+
+        [TestMethod]
+        public void EntityToUserPasswordTest()
+        {
+            User conversion = toTest.ToUser(toGet);
+            Assert.AreEqual(conversion.Password, toGet.Password);
+        }
+
+        [TestMethod]
+        public void EntityToUserEmailTest()
+        {
+            User conversion = toTest.ToUser(toGet);
+            Assert.AreEqual(conversion.Email, toGet.Email);
         }
     }
 }
