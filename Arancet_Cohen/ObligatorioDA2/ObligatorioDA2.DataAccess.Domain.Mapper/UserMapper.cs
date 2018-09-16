5efb14f3c4201ca915aa5200810e2a4dded58ddf
Marcel Cohen <diegomarcel27@hotmail.com>
2018-09-16 16:43:16 -0300

    se escribe el metodo en UserMapper con la minima implementacion para que las pruebas pasen

--- Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.DataAccess.Domain.Mapper/UserMapper.cs
+++ Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.DataAccess.Domain.Mapper/UserMapper.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ObligatorioDA2.DataAccess.Entities;
+using BusinessLogic;
 
 namespace ObligatorioDA2.DataAccess.Domain.Mappers
 {
     public class UserMapper
     {
+        public UserEntity ToEntity(User toConvert)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
