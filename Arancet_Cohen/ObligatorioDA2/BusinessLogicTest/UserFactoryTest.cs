c79e40b68c00ee19de751700fa0c71ddbd9bcb61
Marcel Cohen <diegomarcel27@hotmail.com>
2018-09-17 20:45:40 -0300

    se agrega clase de prueba con tests para una Factory de Users

---
+++ Arancet_Cohen/ObligatorioDA2/BusinessLogicTest/UserFactoryTest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BusinessLogic;
+using System.Collections.Generic;
+
+namespace BusinessLogicTest
+{
+    [TestClass]
+    class UserFactoryTest
+    {
+        User anAdmin;
+        User factoryAdmin;
+        User aFollower;
+        User factoryFollower;
+        UserFactoryTest factory;
+
+        [TestInitialize]
+        public void SetUp() {
+            factory = new UserFactory();
+            anAdmin = new User("name", "surname", "username", "password", "email@domain.com",true);
+            factoryAdmin = factory.CreateAdmin("name", "surname", "username", "password", "email@domain.com");
+            aFollower = new User("name", "surname", "username", "password", "email@domain.com", false);
+            factoryFollower= factory.CreateAdmin("name", "surname", "username", "password", "email@domain.com");
+        }
+
+        [TestMethod]
+        public void CreateAdminNameTest()
+        {
+            Assert.AreEqual(factoryAdmin.Name, anAdmin.Name);
+        }
+
+        [TestMethod]
+        public void CreateAdminSurnameTest()
+        {
+            Assert.AreEqual(factoryAdmin.Surname, anAdmin.Surname);
+        }
+
+        [TestMethod]
+        public void CreateAdminUserNameTest()
+        {
+            Assert.AreEqual(factoryAdmin.UserName, anAdmin.UserName);
+        }
+
+        [TestMethod]
+        public void CreateAdminPasswordTest()
+        {
+            Assert.AreEqual(factoryAdmin.Password, anAdmin.Password);
+        }
+
+        [TestMethod]
+        public void CreateAdminEmailTest()
+        {
+            Assert.AreEqual(factoryAdmin.Email, anAdmin.Email);
+        }
+
+        [TestMethod]
+        public void IsAdminTest() {
+            Assert.IsTrue(factoryAdmin.IsAdmin());
+        }
+
+        [TestMethod]
+        public void IsNotAdmin() {
+            Assert.IsFalse(factoryAdmin.IsAdmin());
+        }
+    }
+}
