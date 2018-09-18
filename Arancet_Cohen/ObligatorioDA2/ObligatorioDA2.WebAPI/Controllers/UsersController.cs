224df3e88fd89d1def09716d26660eafba413cde
Marcel Cohen <diegomarcel27@hotmail.com>
2018-09-18 14:32:15 -0300

    se elimina el Get del controlador de usuarios, no existe necesidad de tener esa funcion

--- Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.WebAPI/Controllers/UsersController.cs
+++ Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.WebAPI/Controllers/UsersController.cs
@@ -22,13 +22,6 @@ namespace ObligatorioDA2.WebAPI.Controllers
             factory = new UserFactory();
         }
 
-        // GET api/values
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
         // GET api/values/5
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
