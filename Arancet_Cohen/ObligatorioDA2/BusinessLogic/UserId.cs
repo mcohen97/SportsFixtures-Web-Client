da1a1fec0c3b9e07157229ac651fe95d9d59abd3
Marcel Cohen <diegomarcel27@hotmail.com>
2018-09-17 21:12:57 -0300

    se agrega estructura de datos definida en la prueba

---
+++ Arancet_Cohen/ObligatorioDA2/BusinessLogic/UserId.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLogic
+{
+    public class UserId
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+    }
+}
