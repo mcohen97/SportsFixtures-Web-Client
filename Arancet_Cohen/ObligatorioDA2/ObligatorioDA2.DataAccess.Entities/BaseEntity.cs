ea674aff023bd4117f204683d97a48101e4a20da
Marcel Cohen <diegomarcel27@hotmail.com>
2018-09-21 22:40:08 -0300

    se agrega la entity base de todas las que persistiran en base de datos

---
+++ Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.DataAccess.Entities/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObligatorioDA2.DataAccess.Entities
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+    }
+}
