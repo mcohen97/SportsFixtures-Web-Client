db439a9a1bd4f3b2dac7edf69211772f07c60201
Marcel Cohen <diegomarcel27@hotmail.com>
2018-09-21 23:11:05 -0300

    se agrega la excepcion para que la prueba compile

---
+++ Arancet_Cohen/ObligatorioDA2/DataRepositories/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataRepositories.Exceptions
+{
+    public class EntityNotFoundException:Exception
+    {
+        public EntityNotFoundException() : base() {
+        }
+    }
+}
