1a543c181b45044c96759052ab4faa616d282230
Marcel Cohen <diegomarcel27@hotmail.com>
2018-09-22 20:39:58 -0300

    se agrega la clase LoginService para que las pruebas compilen y se implementa el metodo probado

---
+++ Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.Services/LoginService.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLogic;
+using DataRepositoryInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObligatorioDA2.Services
+{
+    public class LoginService
+    {
+        private IUserRepository users;
+
+        public LoginService(IUserRepository aRepo)
+        {
+            users = aRepo;
+        }
+
+        public User Login(string aUsername, string aPassword)
+        {
+            User fetched = users.GetUserByUsername(aUsername);
+            return fetched;
+        }
+    }
+}
