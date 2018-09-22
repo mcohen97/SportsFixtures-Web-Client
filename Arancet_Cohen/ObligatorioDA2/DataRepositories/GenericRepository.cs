de258295d48e8c7612b0cd4096baf38c7ba0f906
Marcel Cohen <diegomarcel27@hotmail.com>
2018-09-21 22:47:52 -0300

    Se agrega la clase GnericRepository testeada con las funcionalidades probadas implementadas para que la prueba pase

---
+++ Arancet_Cohen/ObligatorioDA2/DataRepositories/GenericRepository.cs
@@ -0,0 +1,33 @@
+﻿using DataAccess;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using ObligatorioDA2.DataAccess.Entities;
+using RepositoryInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+
+namespace DataRepositories
+{
+    public class GenericRepository<T> : IEntityRepository<T> where T : BaseEntity
+    {
+        private readonly DatabaseConnection context;
+        public GenericRepository(DatabaseConnection aContext)
+        {
+            context = aContext;
+        }
+        public void Add(T entity)
+        {
+            context.Set<T>().Add(entity);
+            context.SaveChanges();
+        }
+
+        public T Get(int id)
+        {
+            return context.Set<T>().First(u=> u.Id == id);
+        }
+    }
+}
