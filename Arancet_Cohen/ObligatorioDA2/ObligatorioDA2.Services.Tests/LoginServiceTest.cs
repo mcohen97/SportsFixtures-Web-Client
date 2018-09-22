4c623770e5db5af04a447b2b915fcb2eb776b6cf
Marcel Cohen <diegomarcel27@hotmail.com>
2018-09-22 20:25:47 -0300

    se crea clase de prueba para servicio de login y se escribe prueba para login exitoso

---
+++ Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.Services.Tests/LoginServiceTest.cs
@@ -0,0 +1,44 @@
+﻿using BusinessLogic;
+using DataRepositoryInterfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace ObligatorioDA2.Services.Tests
+{
+    [TestClass]
+    public class LoginServiceTest
+    {
+
+        private Mock<IUserRepository> repo;
+        private LoginService logger;
+        private User user;
+
+        [TestInitialize]
+        public void SetUp() {
+            repo = new Mock<IUserRepository>();
+            logger = new LoginService(repo.Object);
+            UserId id = new UserId { Name = "aName", Surname = "aSurname",
+                Password = "aPassword", UserName = "aUsername", Email = "anEmail@aDomain.com" };
+
+            user = new User(id, true);
+        }
+
+        [TestMethod]
+        public void LoginSuccesfullyTest() {
+            //arrange
+            repo.Setup(r => r.GetUserByUsername("aUsername")).Returns(user);
+
+            //act
+            User logged = logger.Login("aUsername", "aPassword");
+
+            repo.VerifyAll();
+            Assert.AreEqual(logged.Name,"aName");
+            Assert.AreEqual(logged.Surname,"aSurname");
+            Assert.AreEqual(logged.UserName,"aUsername");
+            Assert.AreEqual(logged.Password,"aPassword");
+            Assert.AreEqual(logged.Email, "anEmail@aDomain.com");
+        }
+
+
+    }
+}
