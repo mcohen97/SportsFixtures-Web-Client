e135cf04684a5155f646624bcadcf6995e6c3fd9
Marcel Cohen <diegomarcel27@hotmail.com>
2018-09-21 22:41:22 -0300

    se agrega interfaz de repositorio que extiende a IRepository. Sirve para repositorios que tratan directamente con las entities

---
+++ Arancet_Cohen/ObligatorioDA2/DataRepositoryInterfaces/IEntityRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using RepositoryInterface;
+
+namespace RepositoryInterface
+{
+    public interface IEntityRepository<T>: IRepository<T>
+    {
+        bool Any(Expression<Func<T, bool>> predicate);
+
+        T First(Expression<Func<T, bool>> predicate);
+
+        ICollection<T> Get(Expression<Func<T, bool>> predicate);
+    }
+}
