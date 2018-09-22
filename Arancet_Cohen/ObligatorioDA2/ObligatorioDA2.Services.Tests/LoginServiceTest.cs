d98f2bd6e7ca3066a9c11f09a791fd930bac258d
Marcel Cohen <diegomarcel27@hotmail.com>
2018-09-22 20:55:54 -0300

    se agregan pruebas para expandir la funcionalidad de login, cubrir mas casos

--- Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.Services.Tests/LoginServiceTest.cs
+++ Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.Services.Tests/LoginServiceTest.cs
@@ -3,6 +3,7 @@ using DataRepositoryInterfaces;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using ObligatorioDA2.Services;
+using ObligatorioDA2.BusinessLogic.Data.Exceptions;
 
 namespace ObligatorioDA2.Services.Tests
 {
@@ -33,13 +34,30 @@ namespace ObligatorioDA2.Services.Tests
             User logged = logger.Login("aUsername", "aPassword");
 
             repo.VerifyAll();
-            Assert.AreEqual(logged.Name,"aName");
-            Assert.AreEqual(logged.Surname,"aSurname");
-            Assert.AreEqual(logged.UserName,"aUsername");
-            Assert.AreEqual(logged.Password,"aPassword");
+            Assert.AreEqual(logged.Name, "aName");
+            Assert.AreEqual(logged.Surname, "aSurname");
+            Assert.AreEqual(logged.UserName, "aUsername");
+            Assert.AreEqual(logged.Password, "aPassword");
             Assert.AreEqual(logged.Email, "anEmail@aDomain.com");
         }
 
+        [TestMethod]
+        [ExpectedException(typeof(UserNotFoundException))]
+        public void UserNotFoundTest() {
+            //arrange.
+            repo.Setup(r => r.GetUserByUsername("otherUsername")).Throws(new UserNotFoundException());
+            //act.
+            logger.Login("otherUsername", "password");
+        }
 
+        [TestMethod]
+        [ExpectedException(typeof(WrongPasswordException))]
+        public void WrongPasswordTest() {
+            //arrange
+            repo.Setup(r => r.GetUserByUsername("aUsername")).Returns(user);
+
+            //act
+            User logged = logger.Login("aUsername", "otherPassword");
+        }
     }
 }
