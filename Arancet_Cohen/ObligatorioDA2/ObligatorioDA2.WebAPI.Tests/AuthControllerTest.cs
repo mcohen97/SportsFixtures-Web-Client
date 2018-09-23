66fafcb110a7f277626e83db018a9a47d09f1db3
Marcel Cohen <diegomarcel27@hotmail.com>
2018-09-23 13:01:45 -0300

    se escribe prueba para metodo login

--- Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.WebAPI.Tests/AuthControllerTest.cs
+++ Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.WebAPI.Tests/AuthControllerTest.cs
@@ -1,9 +1,36 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using ObligatorioDA2.Services;
+using Moq;
+using BusinessLogic;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ObligatorioDA2.WebAPI.Tests
 {
+    [TestClass]
     public class AuthControllerTest
     {
+        private AuthController controllerToTest;
+        private Mock<LoginService> logger;
+        private Mock<User> testUser;
+        [TestInitialize]
+        public void StartUp() {
+            logger = new Mock<LoginService>();
+            UserId identity = new UserId() { Name= "aName", Surname="aUsername", UserName="aUsername",
+                Password= "aPassword", Email= "anEmail@aDomain.com" };
+            testUser = new Mock<User>(identity);
+        }
+
+        [TestMethod]
+        public void LoginSuccesfullyTest() {
+            //arrange
+            logger.Setup(l => l.Login("aUsername", "aPassword")).Returns(testUser.Object);
+
+            //act
+            IActionResult result = controllerToTest.Authenticate("aUsername", "aPassword");
+            OkObjectResult okResult = result as OkObjectResult;
+
+            logger.VerifyAll();
+            Assert.IsNotNull(okResult);
+        }
     }
 }
