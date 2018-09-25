b1596092614866bd16433b0c5d6872d58eb9571a
Marcel Cohen <diegomarcel27@hotmail.com>
2018-09-25 18:55:14 -0300

    se agrega el metodo ToEntity con la minima implementacion para que las pruebas pasen

--- Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.DataAccess.Domain.Mapper/CommentMapper.cs
+++ Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.DataAccess.Domain.Mapper/CommentMapper.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using BusinessLogic;
+using ObligatorioDA2.DataAccess.Entities;
 
 namespace ObligatorioDA2.DataAccess.Domain.Mappers
 {
     public class CommentMapper
     {
+        public CommentEntity ToEntity(Commentary comment)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
