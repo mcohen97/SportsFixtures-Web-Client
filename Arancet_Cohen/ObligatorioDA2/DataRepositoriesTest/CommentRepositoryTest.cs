d59d9f41121fc2fbcedbecc6ec67268601fc17ed
Marcel Cohen <diegomarcel27@hotmail.com>
2018-09-25 10:36:12 -0300

    se crea metodo para inicializar las pruebas del comment repository

--- Arancet_Cohen/ObligatorioDA2/DataRepositoriesTest/CommentRepositoryTest.cs
+++ Arancet_Cohen/ObligatorioDA2/DataRepositoriesTest/CommentRepositoryTest.cs
@@ -1,11 +1,26 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Linq;
+using DataAccess;
+using Microsoft.EntityFrameworkCore;
+using RepositoryInterface;
+using DataRepositories;
 
 namespace DataRepositoriesTest
 {
     [TestClass]
     public class CommentRepositoryTest
     {
+        ICommentRepository comments;
+        
+        [TestInitialize]
+        public void SetUp() {
+            DbContextOptions<DatabaseConnection> options = new DbContextOptionsBuilder<DatabaseConnection>()
+                .UseInMemoryDatabase(databaseName: "CommentRepository")
+                .Options;
+            DatabaseConnection db = new DatabaseConnection(options);
+            IEntityRepository<CommentEntity> entityRepo = new GenericRepository<CommentEntity>();
+        }
+
     }
 }
