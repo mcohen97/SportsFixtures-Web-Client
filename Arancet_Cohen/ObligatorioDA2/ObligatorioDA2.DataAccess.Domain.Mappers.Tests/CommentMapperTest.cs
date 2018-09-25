2b6033576f56917166280e9be497b809be8fa878
Marcel Cohen <diegomarcel27@hotmail.com>
2018-09-25 18:47:13 -0300

    se crea clase de prueba para mapper de Comentario-Entidad

---
+++ Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.DataAccess.Domain.Mappers.Tests/CommentMapperTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace ObligatorioDA2.DataAccess.Domain.Mappers.Tests
+{
+    [TestClass]
+    public class CommentMapperTest
+    {
+        private CommentMapper testMapper;
+        private Comment comment;
+        private CommentEntity commentEntity;
+        [TestInitialize]
+        public void StartUp() {
+            testMapper = new CommentMapper();
+            Mock<User> stub = new Mock<User>("aName", "aSurname", "aUsername", "aPassword", "anEmail@aDomain.com");
+            comment = new Comment("this is a comment", stub.Object);
+        }
+    }
+}
