2739161d32aae3dfd270e52c661ca24267988ebc
Marcel Cohen <diegomarcel27@hotmail.com>
2018-09-25 19:45:31 -0300

    se configura la inicializacion de las pruebas

--- Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.DataAccess.Domain.Mappers.Tests/MatchMapperTest.cs
+++ Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.DataAccess.Domain.Mappers.Tests/MatchMapperTest.cs
@@ -1,10 +1,34 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+﻿using BusinessLogic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using ObligatorioDA2.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
 
 namespace ObligatorioDA2.DataAccess.Domain.Mappers.Tests
 {
     [TestClass]
     public class MatchMapperTest
     {
+        private MatchMapper testMapper;
+        private MatchEntity entity;
+        private Mock<BusinessLogic.Match> match;
+        [TestInitialize]
+        public void SetUp() {
+            testMapper = new MatchMapper();
+            TeamEntity homeTest = new TeamEntity { Id = 3, Name = "Nacional", Photo = "aPath" };
+            TeamEntity awayTest = new TeamEntity { Id = 4, Name = "Torque", Photo = "aPath" };
+            entity = new MatchEntity()
+            {
+                Id = 3,
+                HomeTeam = homeTest,
+                AwayTeam = awayTest,
+                Date = DateTime.Now,
+                Commentaries = new List<CommentEntity>()
+            };
+            Mock<Team> homeMock = new Mock<Team>(3, "Nacional", "aPath");
+            Mock<Team> awayMock = new Mock<Team>(4, "Torque", "aPath");
+            match = new Mock<BusinessLogic.Match>(homeMock.Object,awayMock.Object,DateTime.Now);
+        }
     }
 }
