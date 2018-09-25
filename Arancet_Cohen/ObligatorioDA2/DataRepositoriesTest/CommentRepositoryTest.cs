30e65577269b48d4417ad368a1d276de87bef3c5
Marcel Cohen <diegomarcel27@hotmail.com>
2018-09-25 16:27:34 -0300

    se escriben pruebas para metodo add del repositorio

--- Arancet_Cohen/ObligatorioDA2/DataRepositoriesTest/CommentRepositoryTest.cs
+++ Arancet_Cohen/ObligatorioDA2/DataRepositoriesTest/CommentRepositoryTest.cs
@@ -5,13 +5,17 @@ using DataAccess;
 using Microsoft.EntityFrameworkCore;
 using RepositoryInterface;
 using DataRepositories;
+using Moq;
+using BusinessLogic;
 
 namespace DataRepositoriesTest
 {
     [TestClass]
     public class CommentRepositoryTest
     {
-        ICommentRepository comments;
+        ICommentRepository commentsRepo;
+        Commentary comment;
+        //Mock<BusinessLogic.Match> match;
         
         [TestInitialize]
         public void SetUp() {
@@ -20,6 +24,23 @@ namespace DataRepositoriesTest
                 .Options;
             DatabaseConnection db = new DatabaseConnection(options);
             IEntityRepository<CommentEntity> entityRepo = new GenericRepository<CommentEntity>();
+            commentsRepo = new CommentRepository(entityRepo);
+            comment = new Commentary(4,"this is a comment");
+            commentsRepo.Clear();
+            //Mock<Team> home = new Mock<Team>(3,"River Plate", "aPath");
+            //Mock<Team> away = new Mock<Team>(3, "Boca Juniors", "aPath");
+            //match = new Mock<BusinessLogic.Match>(home.Object, away.Object);
+        }
+
+        [TestMethod]
+        public void EmptyRepositoryTest() {
+            Assert.IsTrue(commentsRepo.IsEmpty());
+        }
+
+        [TestMethod]
+        public void AddCommentTest() {
+            commentsRepo.AddComment(comment);
+            Assert.IsFalse(commentsRepo.IsEmpty());
         }
 
     }
