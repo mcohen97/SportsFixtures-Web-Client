0bbf579b93e885a29c63ce063dc53084b9b63932
Marcel Cohen <diegomarcel27@hotmail.com>
2018-09-25 18:50:34 -0300

    se agregan pruebas para el metodo que pasa de Comentario a su entity

--- Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.DataAccess.Domain.Mappers.Tests/CommentMapperTest.cs
+++ Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.DataAccess.Domain.Mappers.Tests/CommentMapperTest.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+﻿using BusinessLogic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using ObligatorioDA2.DataAccess.Entities;
 
 namespace ObligatorioDA2.DataAccess.Domain.Mappers.Tests
 {
@@ -7,13 +9,26 @@ namespace ObligatorioDA2.DataAccess.Domain.Mappers.Tests
     public class CommentMapperTest
     {
         private CommentMapper testMapper;
-        private Comment comment;
+        private Commentary comment;
         private CommentEntity commentEntity;
         [TestInitialize]
         public void StartUp() {
             testMapper = new CommentMapper();
             Mock<User> stub = new Mock<User>("aName", "aSurname", "aUsername", "aPassword", "anEmail@aDomain.com");
-            comment = new Comment("this is a comment", stub.Object);
+            comment = new Commentary("this is a comment", stub.Object);
+        }
+
+        [TestMethod]
+        public void CommentToEntityTextTest() {
+            CommentEntity entity = testMapper.ToEntity(comment);
+            Assert.AreEqual(entity.Text, comment.Text);
+        }
+
+        [TestMethod]
+        public void CommentToEntityUserTest()
+        {
+            CommentEntity entity = testMapper.ToEntity(comment);
+            Assert.AreEqual(entity.Maker.UserName, comment.Maker);
         }
     }
 }
