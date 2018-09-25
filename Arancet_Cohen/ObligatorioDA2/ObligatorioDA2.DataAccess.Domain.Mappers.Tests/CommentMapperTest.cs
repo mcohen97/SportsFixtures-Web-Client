92a018b501585bb593b9912eebe9a008d03a9cfc
Marcel Cohen <diegomarcel27@hotmail.com>
2018-09-25 19:23:07 -0300

    se crean pruebas para metodo que transforma entidad en comentario

--- Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.DataAccess.Domain.Mappers.Tests/CommentMapperTest.cs
+++ Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.DataAccess.Domain.Mappers.Tests/CommentMapperTest.cs
@@ -24,6 +24,12 @@ namespace ObligatorioDA2.DataAccess.Domain.Mappers.Tests
             };
             Mock<User> stub = new Mock<User>(identity,true);
             comment = new Commentary("this is a comment", stub.Object);
+            commentEntity = new CommentEntity()
+            {
+                Id = 3,
+                Text = "another comment",
+                Maker = new UserEntity() { UserName = "aUsername" }
+            };
         }
 
         [TestMethod]
@@ -43,6 +49,25 @@ namespace ObligatorioDA2.DataAccess.Domain.Mappers.Tests
         public void CommentToEntityIdTest() {
             CommentEntity entity = testMapper.ToEntity(comment);
             Assert.AreEqual(entity.Id, comment.Id);
+        }
+
+        [TestMethod]
+        public void EntityToCommentTextTest() {
+            Commentary  converted = testMapper.ToComment(commentEntity);
+            Assert.AreEqual(converted.Text, commentEntity.Text);
+        }
+
+        [TestMethod]
+        public void EntityToCommentUserTest() {
+            Commentary converted = testMapper.ToComment(commentEntity);
+            Assert.AreEqual(converted.Maker.UserName, commentEntity.Maker.UserName);
+
+        }
+
+        [TestMethod]
+        public void EntityToCommentIdTest() {
+            Commentary converted = testMapper.ToComment(commentEntity);
+            Assert.AreEqual(converted.Id, commentEntity.Id);
         }
     }
 }
