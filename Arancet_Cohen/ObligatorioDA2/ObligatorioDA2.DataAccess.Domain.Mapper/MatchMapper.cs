dfa6403f9aa61e11f6a2540950d7f5d737c43888
Marcel Cohen <diegomarcel27@hotmail.com>
2018-09-25 20:19:54 -0300

    se implementa metodo con la minima implementacion para que las pruebas compilen

--- Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.DataAccess.Domain.Mapper/MatchMapper.cs
+++ Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.DataAccess.Domain.Mapper/MatchMapper.cs
@@ -1,10 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using BusinessLogic;
+using ObligatorioDA2.DataAccess.Entities;
 
 namespace ObligatorioDA2.DataAccess.Domain.Mappers
 {
     public class MatchMapper
     {
+        private TeamMapper teamConverter;
+        private CommentMapper commentConverter;
+
+        public MatchMapper() {
+            teamConverter = new TeamMapper();
+            commentConverter = new CommentMapper();
+        }
+        public MatchEntity ToEntity(Match aMatch)
+        {
+            /* MatchEntity conversion = new MatchEntity() {
+                 HomeTeam
+             }*/
+            return null;
+        }
     }
 }
