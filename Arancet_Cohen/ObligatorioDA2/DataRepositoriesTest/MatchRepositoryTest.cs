35308f5c588daf325973e0d5e708769410365aad
Marcel Cohen <diegomarcel27@hotmail.com>
2018-09-25 18:01:56 -0300

    se agrega metodo de inicializacion de la clase de prueba

--- Arancet_Cohen/ObligatorioDA2/DataRepositoriesTest/MatchRepositoryTest.cs
+++ Arancet_Cohen/ObligatorioDA2/DataRepositoriesTest/MatchRepositoryTest.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using DataAccess;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 
 namespace DataRepositoriesTest
@@ -6,5 +8,17 @@ namespace DataRepositoriesTest
     [TestClass]
     public class MatchRepositoryTest
     {
+        IMatchRepository matchesStorage;
+
+        [TestInitialize]
+        public void SetUp() {
+            DbContextOptions<DatabaseConnection> options = new DbContextOptionsBuilder<DatabaseConnection>()
+                .UseInMemoryDatabase(databaseName: "TeamRepository")
+                .Options;
+            DatabaseConnection context = new DatabaseConnection(options);
+            matchesStorage = new MatchRepository(context);
+            matchesStorage.Clear();
+        }
+
     }
 }
