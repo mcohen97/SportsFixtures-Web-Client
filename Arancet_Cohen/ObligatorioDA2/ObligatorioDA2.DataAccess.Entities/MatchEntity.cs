e612a2abd3ef3642c578123c6052fff6e5069be8
Marcel Cohen <diegomarcel27@hotmail.com>
2018-09-25 18:46:45 -0300

    se crea entidad de partido

---
+++ Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.DataAccess.Entities/MatchEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObligatorioDA2.DataAccess.Entities
+{
+    public class MatchEntity:BaseEntity
+    {
+        public TeamEntity HomeTeam;
+        public TeamEntity AwayTeam;
+        public DateTime Date;
+        public ICollection<CommentEntity> Commentaries;
+    }
+}
