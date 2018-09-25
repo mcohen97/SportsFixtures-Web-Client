10cb39821e8f0e1229638b6515bcfa3d43ed5b60
Marcel Cohen <diegomarcel27@hotmail.com>
2018-09-25 19:29:07 -0300

    se agrega clase de prueba para mapper de equipo

---
+++ Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.DataAccess.Domain.Mappers.Tests/MatchMapperTest.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace ObligatorioDA2.DataAccess.Domain.Mappers.Tests
+{
+    [TestClass]
+    public class MatchMapperTest
+    {
+    }
+}
