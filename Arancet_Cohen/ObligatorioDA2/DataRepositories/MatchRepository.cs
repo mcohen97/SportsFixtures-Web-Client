de8e35559144e875ed6f38ddde7adfc9c4aae68d
Marcel Cohen <diegomarcel27@hotmail.com>
2018-09-25 18:11:20 -0300

    se agrega interfaz match repository, y la implementacion para que las pruebas compilen

---
+++ Arancet_Cohen/ObligatorioDA2/DataRepositories/MatchRepository.cs
@@ -0,0 +1,18 @@
+﻿using DataAccess;
+using DataRepositoryInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataRepositories
+{
+    public class MatchRepository : IMatchRepository
+    {
+        private DatabaseConnection context;
+
+        public MatchRepository(DatabaseConnection context)
+        {
+            this.context = context;
+        }
+    }
+}
---
+++ Arancet_Cohen/ObligatorioDA2/DataRepositoryInterfaces/IMatchRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BusinessLogic;
+using RepositoryInterface;
+
+namespace DataRepositoryInterfaces
+{
+    public interface IMatchRepository:IRepository<Match>
+    {
+
+    }
+}
