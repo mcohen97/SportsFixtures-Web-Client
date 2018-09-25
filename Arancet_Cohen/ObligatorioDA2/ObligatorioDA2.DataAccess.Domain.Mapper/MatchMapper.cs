1fc88874899617a16d1309f3d6e65b1ce250bc2f
Marcel Cohen <diegomarcel27@hotmail.com>
2018-09-25 20:54:30 -0300

    se implementa la funcionalidad para que pasen las pruebas

--- Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.DataAccess.Domain.Mapper/MatchMapper.cs
+++ Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.DataAccess.Domain.Mapper/MatchMapper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using BusinessLogic;
 using ObligatorioDA2.DataAccess.Entities;
@@ -11,16 +12,28 @@ namespace ObligatorioDA2.DataAccess.Domain.Mappers
         private TeamMapper teamConverter;
         private CommentMapper commentConverter;
 
-        public MatchMapper() {
+        public MatchMapper()
+        {
             teamConverter = new TeamMapper();
             commentConverter = new CommentMapper();
         }
         public MatchEntity ToEntity(Match aMatch)
         {
-            /* MatchEntity conversion = new MatchEntity() {
-                 HomeTeam
-             }*/
-            return null;
+            MatchEntity conversion = new MatchEntity()
+            {
+                Id = aMatch.Id,
+                HomeTeam = teamConverter.ToEntity(aMatch.HomeTeam),
+                AwayTeam = teamConverter.ToEntity(aMatch.AwayTeam),
+                Date = aMatch.Date,
+                Commentaries = TransformCommentaries(aMatch.GetAllCommentaries())
+            };
+            return conversion;
+        }
+
+        private ICollection<CommentEntity> TransformCommentaries(ICollection<Commentary> commentaries)
+        {
+            return commentaries.Select(c => commentConverter.ToEntity(c)).ToList();
         }
     }
 }
+
