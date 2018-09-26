17049e7efac634f7fec4225fb1ddb1a3c57b7592
Marcel Cohen <diegomarcel27@hotmail.com>
2018-09-26 19:57:24 -0300

    se implementan las funciones probadas

--- Arancet_Cohen/ObligatorioDA2/DataRepositories/MatchRepository.cs
+++ Arancet_Cohen/ObligatorioDA2/DataRepositories/MatchRepository.cs
@@ -1,5 +1,10 @@
-﻿using DataAccess;
+﻿using BusinessLogic;
+using DataAccess;
 using DataRepositoryInterfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using ObligatorioDA2.DataAccess.Domain.Mappers;
+using ObligatorioDA2.DataAccess.Entities;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,10 +14,68 @@ namespace DataRepositories
     public class MatchRepository : IMatchRepository
     {
         private DatabaseConnection context;
-
-        public MatchRepository(DatabaseConnection context)
+        private MatchMapper mapper;
+        public MatchRepository(DatabaseConnection aContext)
         {
-            this.context = context;
+            context = aContext;
+            mapper = new MatchMapper();
+        }
+
+        public void Add(Match aMatch)
+        {
+            MatchEntity entity = mapper.ToEntity(aMatch);
+            context.Matches.Add(entity);
+            context.Update(entity.HomeTeam);
+            context.Update(entity.AwayTeam);
+            foreach (CommentEntity comment in entity.Commentaries) {
+                context.Update(comment);
+            }
+            context.SaveChanges();
+        }
+
+
+
+        public void Clear()
+        {
+            foreach (MatchEntity match in context.Matches) {
+                context.Matches.Remove(match);
+            }
+            context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Exists(Match record)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Match Get(int anId)
+        {
+            
+            MatchEntity a = context.Matches.First(me => me.Id == anId);
+            
+            Match conversion = mapper.ToMatch(a);
+            return conversion;
+            //return null;
+        }
+
+        public ICollection<Match> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsEmpty()
+        {
+            return !context.Matches.Any();
+        }
+
+        public void Modify(Match entity)
+        {
+            throw new NotImplementedException();
         }
     }
 }
