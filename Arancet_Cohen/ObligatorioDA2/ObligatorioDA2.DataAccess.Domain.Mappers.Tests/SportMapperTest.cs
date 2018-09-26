e1a3022efcdf2b1190ede6d8d9309b1d73ec17cc
Marcel Cohen <diegomarcel27@hotmail.com>
2018-09-25 22:00:00 -0300

    se agrega clase para mapper de sport

---
+++ Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.DataAccess.Domain.Mappers.Tests/SportMapperTest.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace ObligatorioDA2.DataAccess.Domain.Mappers.Tests
+{
+    [TestClass]
+    public class SportMapperTest
+    {
+      
+
+    }
+}
