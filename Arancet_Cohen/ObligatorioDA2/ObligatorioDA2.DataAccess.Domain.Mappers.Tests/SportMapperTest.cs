5b6b48f5f2b063537e1f5ab93d26385cb39b11b7
Marcel Cohen <diegomarcel27@hotmail.com>
2018-09-26 15:13:39 -0300

    se agregan pruebas para metodo de SportMapper que convierte deportes en entidades

--- Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.DataAccess.Domain.Mappers.Tests/SportMapperTest.cs
+++ Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.DataAccess.Domain.Mappers.Tests/SportMapperTest.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using ObligatorioDA2.DataAccess.Entities;
+using System.Collections.Generic;
 
 namespace ObligatorioDA2.DataAccess.Domain.Mappers.Tests
 {
@@ -15,6 +16,27 @@ namespace ObligatorioDA2.DataAccess.Domain.Mappers.Tests
         public void SetUp() {
             testMapper = new SportMapper();
             sport = new Mock<Sport>("Soccer");
+            Mock<Team> aTeam = new Mock<Team>("Nacional", "path");
+            sport.Setup(s => s.GetTeams()).Returns(new List<Team>() {aTeam.Object });
+        }
+
+        [TestMethod]
+        public void SportToEntityNameTest() {
+            SportEntity converted = testMapper.ToEntity(sport.Object);
+            Assert.AreEqual(converted.Name, entity.Name);
+         }
+
+        [TestMethod]
+        public void SportToEntityTeamsCountTest() {
+            SportEntity converted = testMapper.ToEntity(sport.Object);
+            Assert.AreEqual(converted.Teams.Count, 1);
+        }
+
+        [TestMethod]
+        public void SportToEntityTeamsTest() {
+            SportEntity converted = testMapper.ToEntity(sport.Object);
+            TeamEntity toCompare = converted.Teams.GetEnumerator().Current;
+            Assert.AreEqual(toCompare.Name, "Nacional");
         }
 
     }
