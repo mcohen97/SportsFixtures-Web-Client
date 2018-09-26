9e8c0cfb473a96653646d976295b2c3131625efc
Marcel Cohen <diegomarcel27@hotmail.com>
2018-09-25 22:11:07 -0300

    se escribe metodo que inicializa las pruebas

--- Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.DataAccess.Domain.Mappers.Tests/SportMapperTest.cs
+++ Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.DataAccess.Domain.Mappers.Tests/SportMapperTest.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+﻿using BusinessLogic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace ObligatorioDA2.DataAccess.Domain.Mappers.Tests
 {
     [TestClass]
     public class SportMapperTest
     {
-      
+        private SportMapper testMapper;
+        private Mock<Sport> sport;
+        private SportEntity entity;
+        [TestInitialize]
+        public void SetUp() {
+            testMapper = new SportMapper();
+            sport = new Mock<Sport>("Soccer");
+        }
 
     }
 }
