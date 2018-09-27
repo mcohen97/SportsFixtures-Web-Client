46bb3b8d706165b434a7a39541d02ebcf60eac5a
Marcel Cohen <diegomarcel27@hotmail.com>
2018-09-27 14:14:27 -0300

    se escribe la inicializacion de las pruebas de MatchService y se crea la clase

--- Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.Services.Tests/MatchServiceTest.cs
+++ Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.Services.Tests/MatchServiceTest.cs
@@ -1,10 +1,28 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+﻿using System;
+using DataRepositoryInterfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Match = BusinessLogic.Match;
 
 namespace ObligatorioDA2.Services.Tests
 {
     [TestClass]
     public class MatchServiceTest
     {
+        private MatchService serviceToTest;
+        Mock<IMatchRepository> repoDouble;
+        [TestInitialize]
+        public void SetUp() {
+            repoDouble = new Mock<IMatchRepository>();
+            Mock<Match> fakeMatch = BuildMatch();
+            serviceToTest = new MatchService(repoDouble.Object);
+            repoDouble.Setup(r => r.Get(2)).Returns(fakeMatch.Object);
+            //repoDouble.Setup(r => r.Get(It.Is<int>(i => i != 2))).Throws(MatchNotFoundException)
+        }
+
+        private Mock<Match> BuildMatch()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.Services/MatchService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataRepositoryInterfaces;
+
+namespace ObligatorioDA2.Services
+{
+    public class MatchService
+    {
+        private IMatchRepository matchesStorage;
+
+        public MatchService(IMatchRepository aRepo)
+        {
+            matchesStorage = aRepo;
+        }
+    }
+}
