63f54ba06480579193237f24d08596f87535686a
Marcel Cohen <diegomarcel27@hotmail.com>
2018-09-27 18:10:14 -0300

    se agregan pruebas para TeamController

--- Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.WebAPI.Tests/TeamControllerTest.cs
+++ Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.WebAPI.Tests/TeamControllerTest.cs
@@ -2,12 +2,29 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ObligatorioDA2.WebAPI.Models;
 using ObligatorioDA2.WebAPI.Controllers;
 using Microsoft.AspNetCore.Mvc;
+using Moq;
+using RepositoryInterface;
+using BusinessLogic;
+using ObligatorioDA2.BusinessLogic.Data.Exceptions;
 
 namespace ObligatorioDA2.WebAPI.Tests
 {
     [TestClass]
     public class TeamControllerTest
     {
+        private TeamController controller;
+        private Mock<IRepository<Team>> repo;
+        private Mock<Team> team;
+
+        [TestInitialize]
+        public void SetUp() {
+            team = new Mock<Team>(2,"Nacional", "aPath");
+            repo = new Mock<IRepository<Team>>();
+            repo.Setup(r => r.Get(2)).Returns(team.Object);
+            repo.Setup(r => r.Get(It.Is<int>(i=>i != 2))).Throws(new TeamNotFoundException());
+            controller = new TeamController(repo.Object);
+        }
+
         [TestMethod]
         public void CreateValidTeamTest()
         {
@@ -24,6 +41,7 @@ namespace ObligatorioDA2.WebAPI.Tests
             var modelOut = createdResult.Value as TeamModelOut;
 
             //Assert
+            repo.Verify(r => r.Add(It.IsAny<Team>()), Times.Once);
             Assert.IsNotNull(createdResult);
             Assert.AreEqual("GetById", createdResult.RouteName);
             Assert.AreEqual(201, createdResult.StatusCode);
