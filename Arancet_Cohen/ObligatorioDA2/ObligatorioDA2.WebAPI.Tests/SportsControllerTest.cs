dd63a20f218a3137efd605a52a4c0019cbd085f1
Marcel Cohen <diegomarcel27@hotmail.com>
2018-09-28 14:39:09 -0300

    se agrega clase de prueba para SportsController

---
+++ Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.WebAPI.Tests/SportsControllerTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace ObligatorioDA2.WebAPI.Tests
+{
+    [TestClass]
+    public class SportsControllerTest
+    {
+       
+    }
+}
