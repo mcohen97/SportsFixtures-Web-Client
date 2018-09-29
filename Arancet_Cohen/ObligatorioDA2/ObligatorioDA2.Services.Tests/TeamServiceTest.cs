1b1fa4861114e5fcc23f6720f52959500bcb9fc8
Marcel Cohen <diegomarcel27@hotmail.com>
2018-09-29 17:27:25 -0300

    se crea clase de prueba para TeamService

---
+++ Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.Services.Tests/TeamServiceTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BusinessLogic;
+using DataRepositoryInterfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace ObligatorioDA2.Services.Tests
+{
+    public class TeamServiceTest
+    {
+
+        private ITeamService teamService;
+        private Mock<ITeamRepository> mockTeams;
+        private Mock<ISportRepository> mockSports;
+        private Team testTeam;
+
+        [TestMethod]
+        public void SetUp() {
+            mockTeams = new Mock<ITeamRepository>();
+            teamService = new TeamService(mockTeams.Object);
+            testTeam = new Team("aTeam", "aPhoto");
+        }
+
+        [TestMethod]
+        public void AddTeamTest() {
+            Sport played = new Sport("Soccer");
+            teamService.AddTeam(played, testTeam);
+            mockSports.Verify(s => s.Exists(It.IsAny<Sport>()));
+            mockTeams.Verify(t => t.Add(It.IsAny<string>(), It.IsAny<Team>()));
+        }
+
+    }
+}
