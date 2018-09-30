73d085c455d59aeb502270efb5c6082c97ca257e
Marcel Cohen <diegomarcel27@hotmail.com>
2018-09-30 18:04:21 -0300

    se agregan IUserService y UserService que la implementa

---
+++ Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.Services/UserService.cs
@@ -0,0 +1,11 @@
+﻿using ObligatorioDA2.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObligatorioDA2.Services
+{
+    public class UserService:IUserService
+    {
+    }
+}
