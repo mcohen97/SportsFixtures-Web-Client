e4360c4730a3d6c6a0ff19e07da4a47ccfab5f02
Marcel Cohen <diegomarcel27@hotmail.com>
2018-10-02 00:55:09 -0300

    se crea clase API vacia de MatchesController para comenzar a escribir los metodos probados

---
+++ Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.WebAPI/Controllers/MatchesController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace ObligatorioDA2.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class MatchesController : Controller
+    {
+
+    }
+}
