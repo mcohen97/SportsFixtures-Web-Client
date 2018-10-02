111868f17dea96038178817acc47224414079019
Marcel Cohen <diegomarcel27@hotmail.com>
2018-10-02 00:21:11 -0300

    se agrega una interfaz para abstraer los comportamientos del MatchService

---
+++ Arancet_Cohen/ObligatorioDA2/DataRepositoryInterfaces/IMatchService.cs
@@ -0,0 +1,27 @@
+﻿using BusinessLogic;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataRepositoryInterfaces
+{
+    public interface IMatchService
+    {
+        Match AddMatch(Match aMatch);
+
+        ICollection<Match> GetAllMatches();
+
+        Match GetMatch(int anId);
+
+        void DeleteMatch(int anId);
+
+        void ModifyMatch(Match aMatch);
+
+        ICollection<Match> GetAllMatches(Sport sport);
+
+        ICollection<Match> GetAllMatches(Team team);
+
+        bool Exists(int id);
+
+    }
+}
