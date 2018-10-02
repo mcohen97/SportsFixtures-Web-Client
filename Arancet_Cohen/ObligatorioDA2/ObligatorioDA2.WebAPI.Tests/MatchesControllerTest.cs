8e1458a2b9faaa127642e36c45c5bec5b9f5ccb3
Marcel Cohen <diegomarcel27@hotmail.com>
2018-10-02 00:41:16 -0300

    se escribe el metodo de inicializacion de pruebas

--- Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.WebAPI.Tests/MatchesControllerTest.cs
+++ Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.WebAPI.Tests/MatchesControllerTest.cs
@@ -1,10 +1,38 @@
-﻿using System;
+﻿using BusinessLogic;
+using DataRepositoryInterfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Text;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Match = BusinessLogic.Match;
 
 
 namespace ObligatorioDA2.WebAPI.Tests
 {
+    [TestClass]
+    public class MatchesControllerTest{
+
+        private Mock<IMatchService> matchService;
+        private MatchesController controller;
+        private Match testMatch;
+
+        [TestInitialize]
+        public void SetUp() {
+            matchService = new Mock<IMatchService>();
+            testMatch = BuildFakeMatch();
+            controller = new MatchesController(matchService.Object);
+        }
+
+        private Match BuildFakeMatch()
+        {
+            Sport played = new Sport("Football");
+            Team home = new Team("Patriots", "aPhoto", played);
+            Team away = new Team("Falcons", "aPhoto", played);
+            Match built = new Match(home, away, DateTime.Now, played);
+            return built;
+        }
+
+    }
    
 }
