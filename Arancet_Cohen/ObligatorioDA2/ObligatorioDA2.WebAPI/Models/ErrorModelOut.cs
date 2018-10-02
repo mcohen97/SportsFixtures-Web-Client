f1b55f75f459206001366e55d05de6882907f51a
Marcel Cohen <diegomarcel27@hotmail.com>
2018-10-02 17:53:39 -0300

    se crea el ErrorModelOut referenciado desde la prueba, sera utilizado en todos los errores a devolver de ahora en mas

---
+++ Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.WebAPI/Models/ErrorModelOut.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ObligatorioDA2.WebAPI.Models
+{
+    public class ErrorModelOut
+    {
+        public string ErrorMessage { get; set; }
+    }
+}
