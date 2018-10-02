7ffb5d25f9ebf690bb2510e49ef1d17563086150
Marcel Cohen <diegomarcel27@hotmail.com>
2018-10-02 17:46:26 -0300

    se crean MatchModelIn referenciado en la prueba

---
+++ Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.WebAPI/Models/MatchModelIn.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ObligatorioDA2.WebAPI.Models
+{
+    public class MatchModelIn
+    {
+        public string SportName { get; set; }
+        public int HomeTeamId { get; set; }
+        public int AwayTeamId { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
