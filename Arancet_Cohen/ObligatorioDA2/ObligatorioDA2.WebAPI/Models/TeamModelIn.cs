826886498d8fb2763f46effdd07a7ec9ba3993de
Marcel Cohen <diegomarcel27@hotmail.com>
2018-10-02 20:10:35 -0300

    se agrega id a team model in

--- Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.WebAPI/Models/TeamModelIn.cs
+++ Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.WebAPI/Models/TeamModelIn.cs
@@ -6,6 +6,9 @@ namespace ObligatorioDA2.WebAPI.Models
     public class TeamModelIn
     {
         [Required]
+        public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }  
 
         [Required]
