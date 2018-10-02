bfecb6c338b3b01939dfd8f309f65d4ebb7e9a11
Marcel Cohen <diegomarcel27@hotmail.com>
2018-10-02 01:05:26 -0300

    se escribe Get con la minima implementacio para que la prueba compile

--- Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.WebAPI/Controllers/MatchesController.cs
+++ Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.WebAPI/Controllers/MatchesController.cs
@@ -3,13 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
+using ObligatorioDA2.WebAPI.Models;
+using RepositoryInterface;
+using BusinessLogic;
+using BusinessLogic.Factories;
+using ObligatorioDA2.BusinessLogic.Data.Exceptions;
+using DataRepositoryInterfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+using ObligatorioDA2.Services.Interfaces;
 
 namespace ObligatorioDA2.WebAPI.Controllers
 {
     [Route("api/[controller]")]
-    public class MatchesController : Controller
+    [ApiController]
+    public class MatchesController : ControllerBase
     {
+        private IMatchService matchService;
+
+        public MatchesController(IMatchService aService)
+        {
+            matchService = aService;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            throw new NotImplementedException();
+        }
 
     }
 }
