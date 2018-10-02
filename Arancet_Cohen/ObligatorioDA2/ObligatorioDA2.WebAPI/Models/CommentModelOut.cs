15c8fdb770d26ba49880960cf28564f8cf65c32f
Marcel Cohen <diegomarcel27@hotmail.com>
2018-10-02 01:20:30 -0300

    se crea CommentModelOut

---
+++ Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.WebAPI/Models/CommentModelOut.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ObligatorioDA2.WebAPI.Models
+{
+    public class CommentModelOut
+    {
+    }
+}
