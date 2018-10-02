26cbe6d3280919bf4bdc6c5c28312fcecbc7c5bf
Marcel Cohen <diegomarcel27@hotmail.com>
2018-10-02 01:20:15 -0300

    se agregan campos al MatchModelOut

--- Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.WebAPI/Models/MatchModelOut.cs
+++ Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.WebAPI/Models/MatchModelOut.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,14 @@ namespace ObligatorioDA2.WebAPI.Models
 {
     public class MatchModelOut
     {
+        [Required]
+        public string SportName { get; set; }
+        [Required]
+        public int HomeTeamId { get; set; }
+        [Required]
+        public int AwayTeamId { get; set; }
+
+        public ICollection<int> CommentsIds { get; set; } 
+
     }
 }
