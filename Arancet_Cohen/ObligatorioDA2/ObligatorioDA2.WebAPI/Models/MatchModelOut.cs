2a50e05d4499cb4161a09fff422ff010c18716a0
Marcel Cohen <diegomarcel27@hotmail.com>
2018-10-02 23:44:08 -0300

    se agrega campo Date al MatchModelOut, se habia olvidado

--- Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.WebAPI/Models/MatchModelOut.cs
+++ Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.WebAPI/Models/MatchModelOut.cs
@@ -12,8 +12,7 @@ namespace ObligatorioDA2.WebAPI.Models
         public string SportName { get; set; }
         public int HomeTeamId { get; set; }
         public int AwayTeamId { get; set; }
-
-
+        public DateTime Date { get; set; }
         public ICollection<int> CommentsIds { get; set; } 
 
     }
