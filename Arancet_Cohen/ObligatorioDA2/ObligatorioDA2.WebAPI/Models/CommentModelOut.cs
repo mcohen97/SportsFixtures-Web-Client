56d3d7c78b6b28f76cf0ee91eb54c20635974cad
Marcel Cohen <diegomarcel27@hotmail.com>
2018-10-03 22:28:08 -0300

    se le agregan atributos al modelo de salida de comentario

--- Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.WebAPI/Models/CommentModelOut.cs
+++ Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.WebAPI/Models/CommentModelOut.cs
@@ -7,5 +7,10 @@ namespace ObligatorioDA2.WebAPI.Models
 {
     public class CommentModelOut
     {
+        public int Id { get; set; }
+
+        public string Text { get; set; }
+
+        public int MakerId { get; set; }
     }
 }
