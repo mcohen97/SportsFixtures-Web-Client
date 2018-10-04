8be5a78a007e593bc89796d04d6a90320335e042
Marcel Cohen <diegomarcel27@hotmail.com>
2018-10-04 16:22:07 -0300

    se crea OkModelOut referenciado en las pruebas

---
+++ Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.WebAPI/Models/OkModelOut.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ObligatorioDA2.WebAPI.Models
+{
+    public class OkModelOut
+    {
+        public string OkMessage { get; set; }
+    }
+}
