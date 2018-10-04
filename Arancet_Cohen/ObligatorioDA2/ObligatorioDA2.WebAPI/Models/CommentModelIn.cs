751e92c5c7e4567e991afab3cadd6fc8c457ae8c
Marcel Cohen <diegomarcel27@hotmail.com>
2018-10-03 22:27:44 -0300

    se crea modelo de entrada del comentario, referido en la prueba

---
+++ Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.WebAPI/Models/CommentModelIn.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ObligatorioDA2.WebAPI.Models
+{
+    public class CommentModelIn
+    {
+        public string Text { get; set; }
+        public string MakerUsername { get; set; }
+    }
+}
