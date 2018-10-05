6743dffd34943cdae9d567e608582a84a4d8d8ea
Facundo Arancet Iza <facuarancet97@gmail.com>
2018-10-05 19:52:45 -0300

    Se agrega interfaz de FixtureService

---
+++ Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.WebAPI.Tests/FixtureControllerTest.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObligatorioDA2.WebAPI.Tests
+{
+    [TestClass]
+    public class FixtureControllerTest
+    {
+
+    }
+}
