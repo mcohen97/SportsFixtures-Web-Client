b292eec38f8fe31d82d637b14fed23e0a8ad0c2a
Marcel Cohen <diegomarcel27@hotmail.com>
2018-10-05 19:36:44 -0300

    se elimina atributo innecesario de TeamModelIn

--- Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.WebAPI/Models/TeamModelIn.cs
+++ Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.WebAPI/Models/TeamModelIn.cs
@@ -6,9 +6,6 @@ namespace ObligatorioDA2.WebAPI.Models
     public class TeamModelIn
     {
         [Required]
-        public int Id { get; set; }
-
-        [Required]
         public string Name { get; set; }  
 
         [Required]
