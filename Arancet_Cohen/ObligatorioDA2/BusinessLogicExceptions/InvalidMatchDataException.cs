6e95be090b98bc2bf93335c5bbc3714d0646c84c
Marcel Cohen <diegomarcel27@hotmail.com>
2018-10-06 16:35:07 -0300

    refactoring sobre excepciones

--- Arancet_Cohen/ObligatorioDA2/BusinessLogicExceptions/InvalidMatchDataException.cs
+++ Arancet_Cohen/ObligatorioDA2/BusinessLogicExceptions/InvalidMatchDataException.cs
@@ -5,9 +5,6 @@ namespace BusinessLogic.Exceptions
     {
         public InvalidMatchDataExcpetion() { }
         public InvalidMatchDataExcpetion(string message) : base(message) { }
-        public InvalidMatchDataExcpetion(string message, System.Exception inner) : base(message, inner) { }
-        protected InvalidMatchDataExcpetion(
-            System.Runtime.Serialization.SerializationInfo info,
-            System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+
     }
 }
--- Arancet_Cohen/ObligatorioDA2/BusinessLogicExceptions/InvalidTeamDataException.cs
+++ Arancet_Cohen/ObligatorioDA2/BusinessLogicExceptions/InvalidTeamDataException.cs
@@ -3,11 +3,6 @@ namespace BusinessLogic.Exceptions
     [System.Serializable]
     public class InvalidTeamDataException : System.Exception
     {
-        public InvalidTeamDataException() { }
         public InvalidTeamDataException(string message) : base(message) { }
-        public InvalidTeamDataException(string message, System.Exception inner) : base(message, inner) { }
-        protected InvalidTeamDataException(
-            System.Runtime.Serialization.SerializationInfo info,
-            System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
     }
 }
