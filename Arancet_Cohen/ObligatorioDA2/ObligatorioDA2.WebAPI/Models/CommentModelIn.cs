a460742f85afd3a46606648d8b15efe517185bb9
Marcel Cohen <diegomarcel27@hotmail.com>
2018-10-08 13:58:14 -0300

    se eliminan datos innecesarios en el CommentModelIn

--- Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.WebAPI/Models/CommentModelIn.cs
+++ Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.WebAPI/Models/CommentModelIn.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,7 @@ namespace ObligatorioDA2.WebAPI.Models
 {
     public class CommentModelIn
     {
+        [Required]
         public string Text { get; set; }
-        public string MakerUsername { get; set; }
-        public int MatchId { get; set; }
     }
 }
