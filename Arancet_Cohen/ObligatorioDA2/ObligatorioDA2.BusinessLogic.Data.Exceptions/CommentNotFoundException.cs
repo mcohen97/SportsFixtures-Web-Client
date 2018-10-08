ed273a751dcc83da356adb1b22928ebcb667e012
Marcel Cohen <diegomarcel27@hotmail.com>
2018-10-08 16:38:46 -0300

    se modifica excepcion

--- Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.BusinessLogic.Data.Exceptions/CommentNotFoundException.cs
+++ Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.BusinessLogic.Data.Exceptions/CommentNotFoundException.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace ObligatorioDA2.BusinessLogic.Data.Exceptions
 {
-    public class CommentNotFoundException:Exception
+    public class CommentNotFoundException:EntityNotFoundException
     {
         public CommentNotFoundException():base("Couldn't find comment") {
 
