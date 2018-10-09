3391b42737813010d153d507de4a307f6b86d892
Marcel Cohen <diegomarcel27@hotmail.com>
2018-10-09 00:23:42 -0300

    se cubren los casos de desconexion de base de datos

--- Arancet_Cohen/ObligatorioDA2/DataAccess/MatchRepository.cs
+++ Arancet_Cohen/ObligatorioDA2/DataAccess/MatchRepository.cs
@@ -7,6 +7,8 @@ using ObligatorioDA2.DataAccess.Entities;
 using System.Collections.Generic;
 using ObligatorioDA2.BusinessLogic.Data.Exceptions;
 using ObligatorioDA2.Data.Repositories.Interfaces;
+using System.Data.Common;
+using System;
 
 namespace ObligatorioDA2.Data.Repositories
 {
@@ -26,17 +28,32 @@ namespace ObligatorioDA2.Data.Repositories
         public Match Add(Match aMatch)
         {
             Match added;
-            if (!Exists(aMatch.Id))
+            try
             {
                 added = TryAdd(aMatch);
             }
-            else {
-                throw new MatchAlreadyExistsException();
+            catch (DbException)
+            {
+                throw new DataInaccessibleException();
             }
             return added;
         }
 
         private Match TryAdd(Match aMatch)
+        {
+            Match added;
+            if (!Exists(aMatch.Id))
+            {
+                added = AddNew(aMatch);
+            }
+            else
+            {
+                throw new MatchAlreadyExistsException();
+            }
+            return added;
+        }
+
+        private Match AddNew(Match aMatch)
         {
             MatchEntity toAdd = matchConverter.ToEntity(aMatch);
             context.Entry(toAdd).State = EntityState.Added;
@@ -70,6 +87,18 @@ namespace ObligatorioDA2.Data.Repositories
 
         public void Clear()
         {
+            try
+            {
+                TryClear();
+            }
+            catch (DbException)
+            {
+                throw new DataInaccessibleException();
+            }
+        }
+
+        private void TryClear()
+        {
             foreach (MatchEntity match in context.Matches)
             {
                 context.Matches.Remove(match);
@@ -78,6 +107,18 @@ namespace ObligatorioDA2.Data.Repositories
         }
 
         public void Delete(int anId)
+        {
+            try
+            {
+                TryDelete(anId);
+            }
+            catch (DbException)
+            {
+                throw new DataInaccessibleException();
+            }
+        }
+
+        private void TryDelete(int anId)
         {
             if (AnyWithId(anId))
             {
@@ -100,6 +141,20 @@ namespace ObligatorioDA2.Data.Repositories
         public Match Get(int anId)
         {
             Match toReturn;
+            try
+            {
+                toReturn = TryGet(anId);
+            }
+            catch (DbException)
+            {
+                throw new DataInaccessibleException();
+            }
+            return toReturn;
+        }
+
+        private Match TryGet(int anId)
+        {
+            Match toReturn;
             if (AnyWithId(anId))
             {
                 toReturn = GetExistentMatch(anId);
@@ -114,9 +169,9 @@ namespace ObligatorioDA2.Data.Repositories
         private Match GetExistentMatch(int anId)
         {
             MatchEntity entity = context.Matches
-                .Include(m=>m.HomeTeam)
-                .Include(m=>m.AwayTeam)
-                .Include(m=>m.SportEntity)
+                .Include(m => m.HomeTeam)
+                .Include(m => m.AwayTeam)
+                .Include(m => m.SportEntity)
                 .Include(m => m.Commentaries).ThenInclude(c => c.Maker)
                 .First(me => me.Id == anId);
 
@@ -127,11 +182,25 @@ namespace ObligatorioDA2.Data.Repositories
 
         public ICollection<Match> GetAll()
         {
+            ICollection<Match> allOfThem;
+            try
+            {
+                allOfThem = TryGetAll();
+            }
+            catch (DbException)
+            {
+                throw new DataInaccessibleException();
+            }
+            return allOfThem;
+        }
+
+        private ICollection<Match> TryGetAll()
+        {
             IQueryable<MatchEntity> entities = context.Matches
-                .Include(m => m.HomeTeam)
-                .Include(m => m.AwayTeam)
-                .Include(m => m.SportEntity)
-                .Include(m => m.Commentaries).ThenInclude(c => c.Maker);
+                                            .Include(m => m.HomeTeam)
+                                            .Include(m => m.AwayTeam)
+                                            .Include(m => m.SportEntity)
+                                            .Include(m => m.Commentaries).ThenInclude(c => c.Maker);
 
             ICollection<Match> translation = entities.Select(m => matchConverter.ToMatch(m)).ToList();
 
@@ -140,26 +209,55 @@ namespace ObligatorioDA2.Data.Repositories
 
         public bool IsEmpty()
         {
+            bool isEmpty;
+            try
+            {
+                isEmpty = AskIfEmpty();
+            }
+            catch (DbException) {
+                throw new DataInaccessibleException();
+            }
+            return isEmpty;
+        }
+
+        private bool AskIfEmpty()
+        {
             return !context.Matches.Any();
         }
 
         public void Modify(Match aMatch)
         {
+            try
+            {
+                TryModify(aMatch);
+            }
+            catch (DbException) {
+                throw new DataInaccessibleException();
+            }
+        }
+
+        private void TryModify(Match aMatch)
+        {
             if (Exists(aMatch.Id))
             {
-                MatchEntity converted = matchConverter.ToEntity(aMatch);
-                if (context.Matches.Any(m => m.Id == aMatch.Id)) {
-                    MatchEntity old = context.Matches.First(m => m.Id == aMatch.Id);
-                    context.Entry(old).State = EntityState.Detached;
-                }
-
-                context.Entry(converted).State = EntityState.Modified;
-                context.SaveChanges();
+                ModifyExistent(aMatch);
             }
             else
             {
                 throw new MatchNotFoundException();
             }
+        }
+
+        public void ModifyExistent(Match aMatch) {
+            MatchEntity converted = matchConverter.ToEntity(aMatch);
+            if (context.Matches.Any(m => m.Id == aMatch.Id))
+            {
+                MatchEntity old = context.Matches.First(m => m.Id == aMatch.Id);
+                context.Entry(old).State = EntityState.Detached;
+            }
+
+            context.Entry(converted).State = EntityState.Modified;
+            context.SaveChanges();
         }
 
         private bool AnyWithId(int anId)
@@ -169,7 +267,21 @@ namespace ObligatorioDA2.Data.Repositories
 
         public Commentary CommentOnMatch(int idMatch, Commentary aComment)
         {
-            if (!context.Matches.Any(m => m.Id == idMatch)) {
+            Commentary made;
+            try
+            {
+                made = TryComment(idMatch, aComment);
+            }
+            catch (DbException) {
+                throw new DataInaccessibleException();
+            }
+            return made;
+        }
+
+        private Commentary TryComment(int idMatch, Commentary aComment)
+        {
+            if (!context.Matches.Any(m => m.Id == idMatch))
+            {
                 throw new MatchNotFoundException();
             }
             return CommentOnExistingMatch(idMatch, aComment);
@@ -187,10 +299,36 @@ namespace ObligatorioDA2.Data.Repositories
 
         public bool Exists(int id)
         {
+            bool exists;
+            try
+            {
+                exists = AskIfExists(id);
+            }
+            catch (DbException) {
+                throw new DataInaccessibleException();
+            }
+            return exists;
+        }
+
+        private bool AskIfExists(int id)
+        {
             return context.Matches.Any(m => m.Id == id);
         }
 
         public ICollection<Commentary> GetComments()
+        {
+            ICollection<Commentary> allComments;
+            try
+            {
+                allComments = TryGetComments();
+            }
+            catch (DbException) {
+                throw new DataInaccessibleException();
+            }
+            return allComments;
+        }
+
+        private ICollection<Commentary> TryGetComments()
         {
             IQueryable<CommentEntity> allComments = context.Comments.Include(c => c.Maker);
             ICollection<Commentary> conversion = allComments.Select(c => commentConverter.ToComment(c)).ToList();
@@ -199,10 +337,24 @@ namespace ObligatorioDA2.Data.Repositories
 
         public Commentary GetComment(int id)
         {
-            if (!context.Comments.Any(c => c.Id == id)) {
+            Commentary toReturn;
+            try
+            {
+                toReturn = TryGetComment(id);
+            }
+            catch (DbException) {
+                throw new DataInaccessibleException();
+            }
+            return toReturn;
+        }
+
+        private Commentary TryGetComment(int id)
+        {
+            if (!context.Comments.Any(c => c.Id == id))
+            {
                 throw new CommentNotFoundException();
             }
-            CommentEntity retrieved = context.Comments.Include(c =>c.Maker).First(c => c.Id == id);
+            CommentEntity retrieved = context.Comments.Include(c => c.Maker).First(c => c.Id == id);
             return commentConverter.ToComment(retrieved);
         }
     }
