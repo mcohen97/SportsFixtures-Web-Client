20feb91a45a4c18a01f358a98b0f83083e5738e6
Marcel Cohen <diegomarcel27@hotmail.com>
2018-10-10 01:35:22 -0300

    se borran constructores no utilizados

--- Arancet_Cohen/ObligatorioDA2/BusinessLogic/Exceptions/InvalidTeamCountException.cs
+++ Arancet_Cohen/ObligatorioDA2/BusinessLogic/Exceptions/InvalidTeamCountException.cs
@@ -2,12 +2,7 @@
 {
     public class InvalidTeamCountException : System.Exception
     {
-        public InvalidTeamCountException() { }
         public InvalidTeamCountException(string message) : base(message) { }
-        public InvalidTeamCountException(string message, System.Exception inner) : base(message, inner) { }
-        protected InvalidTeamCountException(
-          System.Runtime.Serialization.SerializationInfo info,
-          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
     }
 
 }
