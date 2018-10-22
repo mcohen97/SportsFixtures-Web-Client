2eb72cfa02759fffa6f82d4d38345ccdfd78c2c9
Marcel Cohen <diegomarcel27@hotmail.com>
2018-10-22 17:43:32 -0300

    se agrega clase de prueba para nuevo Controller especializado en fixtures

---
+++ Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.WebAPI.Tests/FixtureControllerTest.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace ObligatorioDA2.WebAPI.Tests
+{
+    [TestClass]
+    public class FixtureControllerTest
+    {
+        [TestInitialize]
+        public void SetUp() {
+        }
+    }
+}
