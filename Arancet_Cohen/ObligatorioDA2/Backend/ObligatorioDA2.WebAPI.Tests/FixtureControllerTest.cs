edca5d573189f25c3b814fd7a9d7feaea06e4833
Marcel Cohen <diegomarcel27@hotmail.com>
2018-10-22 20:33:52 -0300

    se agrega codigo en el metodo TestInitialize

--- Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.WebAPI.Tests/FixtureControllerTest.cs
+++ Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.WebAPI.Tests/FixtureControllerTest.cs
@@ -1,13 +1,20 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+﻿using Microsoft.Extensions.Options;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using ObligatorioDA2.Services.Interfaces;
 
 namespace ObligatorioDA2.WebAPI.Tests
 {
     [TestClass]
     public class FixtureControllerTest
     {
+        private FixtureController controller;
+        private Mock<IFixtureService> fixtureService;
+        private Mock<IOptions<FixtureStrategies>> settings;
+
         [TestInitialize]
         public void SetUp() {
+            FixtureControllerTest = new FixtureController(fixtureService, settings);
         }
     }
 }
