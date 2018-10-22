aebffefe6b6051af824641f2e1ddcccaf519b1c1
Marcel Cohen <diegomarcel27@hotmail.com>
2018-10-22 17:37:54 -0300

    se agrega clase FixtureStrategies, un DTO que contiene la configuracion de las strategies

---
+++ Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.WebAPI/FixtureStrategies.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ObligatorioDA2.WebAPI
+{
+    public class FixtureStrategies
+    {
+       public string DllPath { get; set; }
+    }
+}
