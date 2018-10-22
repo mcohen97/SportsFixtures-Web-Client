12c0606cdd80072981d46210a56da65497236513
Marcel Cohen <diegomarcel27@hotmail.com>
2018-10-22 20:39:17 -0300

    se corrijen errores en el codigo de la prueba

--- Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.WebAPI.Tests/FixtureControllerTest.cs
+++ Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.WebAPI.Tests/FixtureControllerTest.cs
@@ -2,19 +2,20 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using ObligatorioDA2.Services.Interfaces;
+using ObligatorioDA2.WebAPI.Controllers;
 
 namespace ObligatorioDA2.WebAPI.Tests
 {
     [TestClass]
     public class FixtureControllerTest
     {
-        private FixtureController controller;
+        private FixturesController controller;
         private Mock<IFixtureService> fixtureService;
         private Mock<IOptions<FixtureStrategies>> settings;
 
         [TestInitialize]
         public void SetUp() {
-            FixtureControllerTest = new FixtureController(fixtureService, settings);
+            controller = new FixturesController(fixtureService.Object, settings.Object);
         }
     }
 }
