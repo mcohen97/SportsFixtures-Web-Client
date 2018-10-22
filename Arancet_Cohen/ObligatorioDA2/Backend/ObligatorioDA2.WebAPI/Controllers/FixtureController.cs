60632ad4d9b8ce2f3df62645104fdbb19cdf0d4b
Marcel Cohen <diegomarcel27@hotmail.com>
2018-10-22 20:39:33 -0300

    se crea controlador referenciado desde la prueba

---
+++ Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.WebAPI/Controllers/FixtureController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ObligatorioDA2.Services.Interfaces;
+using Microsoft.Extensions.Options;
+
+namespace ObligatorioDA2.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FixturesController : ControllerBase
+    {
+        private IFixtureService fixtureService;
+        private IOptions<FixtureStrategies> fixtureConfig;
+
+        public FixturesController(IFixtureService service, IOptions<FixtureStrategies> config) {
+
+        }
+    }
+}
