2d06e9d4b2a8083d3d21e2cda0b8bc318491f534
Facundo Arancet Iza <facuarancet97@gmail.com>
2018-10-24 17:50:06 -0300

    Se define la interface de LoggerService

--- Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.Services.Interfaces/ILoggerService.cs
+++ Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.Services.Interfaces/ILoggerService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ObligatorioDA2.BusinessLogic;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +7,8 @@ namespace ObligatorioDA2.Services.Interfaces
 {
     public interface ILoggerService
     {
-        
+        int Log(string logType, string messagge, string username, DateTime date);
+        LogInfo GetLog(int id);
+        ICollection<LogInfo> GetAllLogs();
     }
 }
