6ebc6d40bc7c57ba8fbb7de6dd464a4a9f47a00f
Facundo Arancet Iza <facuarancet97@gmail.com>
2018-10-24 18:50:31 -0300

    Se crea la clase LogService

---
+++ Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.Services/LoggerService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using ObligatorioDA2.BusinessLogic;
+using ObligatorioDA2.Data.Repositories.Interfaces;
+using ObligatorioDA2.Services.Interfaces;
+
+namespace ObligatorioDA2.Services
+{
+    public class LoggerService : ILoggerService
+    {
+        private ILogInfoRepository logRepo;
+
+        public LoggerService(ILogInfoRepository logRepo)
+        {
+            this.logRepo = logRepo;
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Exists(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<LogInfo> GetAllLogs()
+        {
+            throw new NotImplementedException();
+        }
+
+        public LogInfo GetLog(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Log(string logType, string messagge, string username, DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
