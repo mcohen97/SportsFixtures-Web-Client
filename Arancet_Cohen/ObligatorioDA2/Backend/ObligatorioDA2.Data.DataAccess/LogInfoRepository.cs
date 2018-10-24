cadf777dfbc4b36da1d747909a7e9159af16944a
Facundo Arancet Iza <facuarancet97@gmail.com>
2018-10-24 15:49:19 -0300

    Se genera la clase LogInfoRepository

---
+++ Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.Data.DataAccess/LogInfoRepository.cs
@@ -0,0 +1,14 @@
+﻿using ObligatorioDA2.Data.Repositories.Interfaces;
+
+namespace ObligatorioDA2.Data.DataAccess
+{
+    public class LogInfoRepository : ILogInfoRepository
+    {
+        private DatabaseConnection context;
+
+        public LogInfoRepository(DatabaseConnection context)
+        {
+            this.context = context;
+        }
+    }
+}
