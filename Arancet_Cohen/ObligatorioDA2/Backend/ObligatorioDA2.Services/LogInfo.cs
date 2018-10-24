7050e17c742dad8b538760668bf1aa6d1fe684c1
Facundo Arancet Iza <facuarancet97@gmail.com>
2018-10-24 14:34:27 -0300

    Se crea estructura de datos de log

---
+++ Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.Services/LogInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObligatorioDA2.Services
+{
+    public class LogInfo
+    {
+        private int id;
+        private string messagge;
+        private string logType;
+        private DateTime date;
+
+        public int Id { get; set; }
+        public string Messagge { get; set; }
+        public string LogType { get; set; }
+        public DateTime Date { get; set; }
+
+        public LogInfo() { }
+
+        public LogInfo(string messagge, string type, DateTime date)
+        {
+            Messagge = messagge;
+            LogType = type;
+            Date = date;
+        }
+    }
+}
---
+++ Arancet_Cohen/ObligatorioDA2/ObligatorioDA2.Services/LogInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObligatorioDA2.Services
+{
+    public class LogInfo
+    {
+        public string Action { get; set; }
+        public string UserName { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
