f4a14a4c97e221051b812fe1bebc02c8668947ee
Facundo Arancet Iza <facuarancet97@gmail.com>
2018-10-24 14:50:46 -0300

    Se crea clase de literales para tipos de log

---
+++ Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.DataAccess.Domain.Mappers.Tests/LogInfoMapperTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ObligatorioDA2.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObligatorioDA2.Data.DataAccess.Mappers.Tests
+{
+    [TestClass]
+    public class LogInfoMapperTest
+    {
+        private LogInfoMapper mapper;
+        private LogInfoEntity entity;
+        private LogInfo log;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            mapper = new LogInfoMapper();
+        }
+    }
+}
---
+++ Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.Services/LogTypes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObligatorioDA2.Services
+{
+    public class LogTypes
+    {
+        public static string LOGIN = "Login action";
+        public static string FIXTURE = "Fixture action";
+    }
+}
