97bca5897d4f6a3e9de0498d3f3c2e98f9a44bcf
Facundo Arancet Iza <facuarancet97@gmail.com>
2018-10-24 14:44:07 -0300

    Se crea entidad de estructura de datos loginfo

---
+++ Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.DataAccess.Entities/LogInfoEntity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObligatorioDA2.Data.DataAccess.Entities
+{
+    public class LogInfoEntity
+    {
+        public int Id { get; set; }
+        public string Messagge { get; set; }
+        public string LogType { get; set; }
+        public DateTime Date { get; set; }
+        public string Username { get; set; }
+
+        public LogInfoEntity() { }
+
+        public LogInfoEntity(string messagge, string type, DateTime date, string username)
+        {
+            Id = 0;
+            Messagge = messagge;
+            LogType = type;
+            Date = date;
+            Username = username;
+        }
+    }
+}
