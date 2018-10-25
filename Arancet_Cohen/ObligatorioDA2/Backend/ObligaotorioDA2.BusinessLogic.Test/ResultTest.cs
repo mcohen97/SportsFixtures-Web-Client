a15f835f8b9686e4d576507e77fbc88c23bc583a
Marcel Cohen <diegomarcel27@hotmail.com>
2018-10-25 18:09:45 -0300

    se agrega la clase de prueba para la entidad resultado de partido que se modelara

---
+++ Arancet_Cohen/ObligatorioDA2/Backend/ObligaotorioDA2.BusinessLogic.Test/ResultTest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObligatorioDA2.BusinessLogic.Test
+{
+    class ResultTest
+    {
+    }
+}
---
+++ Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.BusinessLogic/Result.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObligatorioDA2.BusinessLogic
+{
+    public class Result
+    {
+        public ICollection<Tuple<Team,int>> Positions { get; set; }
+    }
+}
