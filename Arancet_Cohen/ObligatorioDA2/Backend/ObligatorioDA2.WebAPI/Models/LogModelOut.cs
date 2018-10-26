0cad39cc059549c3e37f02c7866cd874465b150c
Facundo Arancet Iza <facuarancet97@gmail.com>
2018-10-26 05:34:24 -0300

    Se crea model out de log

---
+++ Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.WebAPI/Models/LogModelOut.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace ObligatorioDA2.WebAPI.Models
+{
+    public class LogModelOut
+    {
+        public int Id { get; set; }
+        public string LogType { get; set; }
+        public string Message { get; set; }
+        public string Useranme { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
