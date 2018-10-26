3ce0edecab8055e254a6ec051053f50b5d1a44e0
Facundo Arancet Iza <facuarancet97@gmail.com>
2018-10-26 05:28:01 -0300

    Se cambia mensaje de login exitoso

--- Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.BusinessLogic/LogMessage.cs
+++ Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.BusinessLogic/LogMessage.cs
@@ -6,9 +6,9 @@ namespace ObligatorioDA2.BusinessLogic
 {
     public  class LogMessage
     {
-        public static readonly string LOGIN_OK = "Logged successful";
-        public static readonly string LOGIN_USER_NOT_FOUND = "Login failed. User not found";
-        public static readonly string LOGIN_WRONG_PASSWORD = "Login failed. Wrong password";
-        public static readonly string LOGIN_DATAINACCESSIBLE = "Login failed. Data inaccessible";
+        public static readonly string LOGIN_OK = "Successful login.";
+        public static readonly string LOGIN_USER_NOT_FOUND = "Login failed. User not found.";
+        public static readonly string LOGIN_WRONG_PASSWORD = "Login failed. Wrong password.";
+        public static readonly string LOGIN_DATAINACCESSIBLE = "Login failed. Data inaccessible.";
     }
 }
