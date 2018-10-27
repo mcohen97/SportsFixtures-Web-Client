6da02fac9593a1732e47f38e02a93e19bc03e75d
Marcel Cohen <diegomarcel27@hotmail.com>
2018-10-27 17:20:51 -0300

    se crea clase de prueba para ImageService

---
+++ Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.Services.Tests/ImageServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ObligatorioDA2.Services.Tests
+{
+    [TestClass]
+    public class ImageServiceTest
+    {
+        private IImageService service;
+
+        [TestInitialize]
+        public void SetUp() {
+            service = new ImageService()
+        }
+    }
+}
