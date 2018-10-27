0159874bb582f08e007552fa06f6cf4302dbbc9a
Marcel Cohen <diegomarcel27@hotmail.com>
2018-10-27 17:58:12 -0300

    se agrega prueba para metodo de leer imagen

--- Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.Services.Tests/ImageServiceTest.cs
+++ Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.Services.Tests/ImageServiceTest.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ObligatorioDA2.Services.Interfaces;
+using System;
 using System.IO;
+using System.Text;
 
 namespace ObligatorioDA2.Services.Tests
 {
@@ -8,26 +10,41 @@ namespace ObligatorioDA2.Services.Tests
     public class ImageServiceTest
     {
         private IImageService service;
-        string directoryPath;
+        private string directoryPath;
+        private string testImagePath;
+        private string testImage;
 
         [TestInitialize]
         public void SetUp() {
             directoryPath = "TestDirectory";
             service = new ImageService(directoryPath);
+            testImagePath = directoryPath + "/" + "testImage.jpg";
+            testImage = "asdasdasdadad"; 
         }
 
         [TestMethod]
         public void TestSaveImage() {
-            string img = "asdasdasdadad";
-            service.SaveImage("testImage", Base64Encode(img));
-            string imagePath= directoryPath +"/"+ "testImage.jpg";
-            Assert.IsTrue(File.Exists(imagePath));
+            SaveTestImage();
+            Assert.IsTrue(File.Exists(testImagePath));
         }
 
         private string Base64Encode(string plainText)
         {
-            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
             return System.Convert.ToBase64String(plainTextBytes);
+        }
+
+        [TestMethod]
+        public void TestReadImage() {
+            SaveTestImage();
+            byte[] bytes= service.ReadImage("testImage");
+            string actual = Convert.ToBase64String(bytes);
+            string expected = Base64Encode(testImage);
+        }
+
+        private void SaveTestImage()
+        {
+            service.SaveImage("testImage", Base64Encode(testImage));
         }
     }
 }
