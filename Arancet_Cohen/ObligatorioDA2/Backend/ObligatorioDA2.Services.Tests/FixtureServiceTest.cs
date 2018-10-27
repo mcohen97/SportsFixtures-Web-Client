0e5e530e1855f77d6c4323c05c716fe955e891fa
Marcel Cohen <diegomarcel27@hotmail.com>
2018-10-26 23:59:06 -0300

    se cambia ruta entera en prueba por ruta relativa

--- Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.Services.Tests/FixtureServiceTest.cs
+++ Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.Services.Tests/FixtureServiceTest.cs
@@ -162,7 +162,7 @@ namespace ObligatorioDA2.Services.Tests
 
         [TestMethod]
         public void GetAvailableStrategiesTest() {
-            string path = "C://Users//Marcel//Desktop//diseño2//Arancet_Cohen//ObligatorioDA2//Backend//ObligatorioDA2.BusinessLogic//bin//Debug//";
+            string path = "..//..//..//..//ObligatorioDA2.BusinessLogic//bin//Debug//";
             ICollection<Type> algorithms = fixtureService.GetAlgorithms(path);
             Assert.AreEqual(0, algorithms.Count);
         }
