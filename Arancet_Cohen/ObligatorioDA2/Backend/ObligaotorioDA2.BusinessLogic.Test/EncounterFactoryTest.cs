93c69c8c141f53896719b5d1eff4c4bb3590ebaf
Marcel Cohen <diegomarcel27@hotmail.com>
2018-10-27 20:53:16 -0300

    se crea clase de prueba para MatchFactory

---
+++ Arancet_Cohen/ObligatorioDA2/Backend/ObligaotorioDA2.BusinessLogic.Test/EncounterFactoryTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ObligatorioDA2.BusinessLogic.Test
+{
+    [TestClass]
+    public class EncounterFactoryTest
+    {
+        private MatchFactory testFactory;
+
+        [TestInitialize]
+        public void SetUp() {
+            testFactory = new MatchFactory();
+        }
+
+
+    }
+}
