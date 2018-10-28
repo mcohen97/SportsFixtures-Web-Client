943a0717706f93757e88e86b44b2275e2e934714
Marcel Cohen <diegomarcel27@hotmail.com>
2018-10-27 21:15:57 -0300

    se agregan pruebas para constructor con 3 parametros

--- Arancet_Cohen/ObligatorioDA2/Backend/ObligaotorioDA2.BusinessLogic.Test/EncounterFactoryTest.cs
+++ Arancet_Cohen/ObligatorioDA2/Backend/ObligaotorioDA2.BusinessLogic.Test/EncounterFactoryTest.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
 
 namespace ObligatorioDA2.BusinessLogic.Test
 {
@@ -7,11 +9,59 @@ namespace ObligatorioDA2.BusinessLogic.Test
     {
         private EncounterFactory testFactory;
 
+        private Team teamA;
+        private Team teamB;
+        private Team teamC;
+        private Sport sport;
+        private DateTime date;
+        private Encounter match;
+        private Commentary commentary1;
+        private Commentary commentary2;
+        private Commentary commentary3;
+
         [TestInitialize]
         public void SetUp() {
             testFactory = new EncounterFactory();
+
+            sport = new Sport("Soccer", true);
+            teamA = new Team(1, "TeamA", "Photo/A", sport);
+            teamB = new Team(2, "TeamB", "Photo/B", sport);
+            teamC = new Team(2, "TeamC", "Photo/C", sport);
+            date = new DateTime(2019, 1, 25, 13, 30, 0);
+            User commentarist = CreateUser();
+            commentary1 = new Commentary(1, "Commentary 1", commentarist);
+            commentary2 = new Commentary(2, "Commentary 2", commentarist);
+            commentary3 = new Commentary(3, "Commentary 3", commentarist);
         }
 
+        private User CreateUser()
+        {
+            UserId identity = new UserId()
+            {
+                Name = "aName",
+                Surname = "aSurname",
+                UserName = "aUsername",
+                Password = "aPassword",
+                Email = "anEmail@aDomain.com"
+            };
+            User toReturn = new User(identity, false);
+            return toReturn;
+        }
+
+        [TestMethod]
+        public void BuildMatch3ArgumentsTest() {
+            Encounter testMatch = testFactory.CreateEncounter(new List<Team>() { teamA, teamB }, date, sport);
+            Match downcast = testMatch as Match;
+            Assert.IsNotNull(downcast);
+        }
+
+        [TestMethod]
+        public void BuildCompetition3ArgumentsTest() {
+            sport = new Sport("Archery", false);
+            Encounter testMatch = testFactory.CreateEncounter(new List<Team>() { teamA, teamB }, date, sport);
+            Competition downcast = testMatch as Competition;
+            Assert.IsNotNull(downcast);
+        }
 
     }
 }
