6e622ae00d9ba0ff1ed0adbf73ecbf7a7da80beb
Marcel Cohen <diegomarcel27@hotmail.com>
2018-10-27 21:16:47 -0300

    se escribe metodo con la minima implementacion para que las pruebas compilen

--- Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.BusinessLogic/EncounterFactory.cs
+++ Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.BusinessLogic/EncounterFactory.cs
@@ -6,6 +6,9 @@ namespace ObligatorioDA2.BusinessLogic
 {
     public class EncounterFactory
     {
-
+        public Encounter CreateEncounter(List<Team> list, DateTime date, Sport sport)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
