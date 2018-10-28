9822daafdc44a116a903ac9865c674bbb77bf2fc
Marcel Cohen <diegomarcel27@hotmail.com>
2018-10-28 14:08:02 -0300

    fix de base de datos

---
+++ Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.Data.DataAccess/Migrations/20181028165838_integration.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccess.Migrations
+{
+    public partial class integration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Position",
+                table: "MatchTeams",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "HasResult",
+                table: "Matches",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateTable(
+                name: "Logs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Messagge = table.Column<string>(nullable: true),
+                    LogType = table.Column<string>(nullable: true),
+                    Date = table.Column<DateTime>(nullable: false),
+                    Username = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Logs", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Logs");
+
+            migrationBuilder.DropColumn(
+                name: "Position",
+                table: "MatchTeams");
+
+            migrationBuilder.DropColumn(
+                name: "HasResult",
+                table: "Matches");
+        }
+    }
+}
