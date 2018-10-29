91f82ec7ea45cf555e8ba0ed434c4607811e0fd3
Marcel Cohen <diegomarcel27@hotmail.com>
2018-10-29 15:22:44 -0300

    se implementa la funcionalidad parcialmente para que la prueba pase

--- Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.Services/SportTableService.cs
+++ Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.Services/SportTableService.cs
@@ -3,6 +3,7 @@ using ObligatorioDA2.Data.Repositories.Interfaces;
 using ObligatorioDA2.Services.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace ObligatorioDA2.Services
@@ -22,7 +23,29 @@ namespace ObligatorioDA2.Services
 
         public ICollection<Tuple<Team, int>> GetScoreTable(string sportName)
         {
-            throw new NotImplementedException();
+            Sport asked = sportsStorage.Get(sportName);
+            ICollection<Team> sportTeams = teamsStorage.GetTeams(sportName);
+            ICollection<Encounter> teamsEncounters = matchesService.GetAllMatches(sportName);
+            ICollection<Tuple<Team, int>> positions = CalculateCompetitionsTable(sportTeams, teamsEncounters);          
+            return positions;
         }
+
+        private ICollection<Tuple<Team, int>> CalculateCompetitionsTable(ICollection<Team> sportTeams, ICollection<Encounter> teamsEncounters)
+        {
+            Dictionary<Team, int> table = sportTeams.ToDictionary(item => item, item => 0);
+            foreach (Encounter game in teamsEncounters){
+                List<Tuple<Team, int>> positions = game.Result.GetPositions().ToList();
+                positions.Sort((x, y) => x.Item2.CompareTo(y.Item2));
+
+                table[positions[0].Item1] += 3;
+                table[positions[1].Item1] += 2;
+                if (positions.Count > 2)
+                {
+                    table[positions[2].Item1] += 1;
+                }
+            }
+            return table.Keys.Select(t => new Tuple<Team, int>(t, table[t])).ToList();
+        }
+
     }
 }
