adda999f522f6844a59597c46a3cb5fb159def84
Marcel Cohen <diegomarcel27@hotmail.com>
2018-10-28 22:41:14 -0300

    se crea la interfaz y la implementacion del servicio

---
+++ Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.Data.Repositories.Interfaces/ISportTableService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObligatorioDA2.Data.Repositories.Interfaces
+{
+    public interface ISportTableService
+    {
+    }
+}
---
+++ Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.Services/SportTableService.cs
@@ -0,0 +1,11 @@
+﻿using ObligatorioDA2.Data.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObligatorioDA2.Services
+{
+    public class SportTableService: ISportTableService
+    {
+    }
+}
