34f8bec8c812048a33ed52b4a727001afde49005
Marcel Cohen <diegomarcel27@hotmail.com>
2018-10-28 23:28:01 -0300

    se termina de escribir, lo que fowler llama "fixture", de las pruebas

--- Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.Services.Tests/SportTableServiceTest.cs
+++ Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.Services.Tests/SportTableServiceTest.cs
@@ -5,6 +5,8 @@ using Moq;
 using ObligatorioDA2.BusinessLogic;
 using ObligatorioDA2.BusinessLogic.Data.Exceptions;
 using ObligatorioDA2.Data.Repositories.Interfaces;
+using ObligatorioDA2.Services.Interfaces;
+using Match = ObligatorioDA2.BusinessLogic.Match;
 
 namespace ObligatorioDA2.Services.Tests
 {
@@ -14,12 +16,14 @@ namespace ObligatorioDA2.Services.Tests
         private SportTableService serviceToTest;
         private Mock<ISportRepository> sportsStorage;
         private Mock<ITeamRepository> teamsStorage;
+        private Mock<IMatchService> matchesService;
 
         [TestInitialize]
         public void SetUp() {
             serviceToTest = new SportTableService();
             sportsStorage = new Mock<ISportRepository>();
             teamsStorage = new Mock<ITeamRepository>();
+            matchesService= new Mock<IMatchService>();
             SetUpSports();
             SetUpTeams();
         }
@@ -37,17 +41,69 @@ namespace ObligatorioDA2.Services.Tests
         private void SetUpTeams()
         {
             Sport twoTeamSport = new Sport("Soccer", true);
-            Team teamA = new Team("teamA", "photoA", twoTeamSport);
-            Team teamB = new Team("teamA", "photoA", twoTeamSport);
-            Team teamC = new Team("teamA", "photoA", twoTeamSport);
+            Team teamA = new Team(1,"teamA", "photoA", twoTeamSport);
+            Team teamB = new Team(2,"teamA", "photoA", twoTeamSport);
+            Team teamC = new Team(3,"teamA", "photoA", twoTeamSport);
+            ICollection<Encounter> matches = CreateMatches(teamA, teamB, teamC,twoTeamSport);
+            matchesService.Setup(r => r.GetAllMatches("Soccer")).Returns(matches);
 
             Sport multipleTeamSport = new Sport("Archery", false);
-            Team teamD = new Team("teamD", "photoD", multipleTeamSport);
-            Team teamE = new Team("teamE", "photoE", multipleTeamSport);
-            Team teamF = new Team("teamF", "photoF", multipleTeamSport);
+            Team athleteD = new Team(4,"teamD", "photoD", multipleTeamSport);
+            Team athleteE = new Team(5,"teamE", "photoE", multipleTeamSport);
+            Team athleteF = new Team(6,"teamF", "photoF", multipleTeamSport);
+            ICollection<Encounter> competitions = CreateCompetitions(athleteD, athleteE,athleteF,multipleTeamSport);
+            matchesService.Setup(r => r.GetAllMatches("Archery")).Returns(competitions);
 
             teamsStorage.Setup(r => r.GetTeams("Soccer")).Returns(new List<Team>() { teamA, teamB, teamC });
-            teamsStorage.Setup(r => r.GetTeams("Archery")).Returns(new List<Team>() { teamD, teamE, teamF });
+            teamsStorage.Setup(r => r.GetTeams("Archery")).Returns(new List<Team>() { athleteD, athleteE, athleteF });
+        }
+
+        private ICollection<Encounter> CreateMatches(Team teamA, Team teamB, Team teamC,Sport played)
+        {
+            //Create matches.
+            Match matchAvsB = new Match(new List<Team>() {teamA,teamB },DateTime.Now,played);
+            Match matchAvsC = new Match(new List<Team>() { teamA, teamC }, DateTime.Now, played);
+            Match matchBvsC = new Match(new List<Team>() { teamB, teamC }, DateTime.Now, played);
+            //Create results.
+            Result resultAvsB = new Result();
+            resultAvsB.Add(teamA, 1);
+            resultAvsB.Add(teamB, 2);
+            Result resultAvsC = new Result();
+            resultAvsC.Add(teamA, 2);
+            resultAvsC.Add(teamC, 1);
+            Result resultBvsC = new Result();
+            resultBvsC.Add(teamB, 1);
+            resultBvsC.Add(teamC, 1);
+            //Assign results to matches.
+            matchAvsB.Result = resultAvsB;
+            matchAvsC.Result = resultAvsC;
+            matchBvsC.Result = resultBvsC;
+
+            return new List<Encounter>() { matchAvsB, matchAvsC, matchBvsC };
+        }
+        private ICollection<Encounter> CreateCompetitions(Team teamD, Team teamE, Team teamF, Sport played)
+        {
+            //Create competition.
+            Match competitiomDEF = new Match(new List<Team>() { teamD, teamE, teamF }, DateTime.Now, played);
+            Match competitionED = new Match(new List<Team>() { teamE, teamD }, DateTime.Now, played);
+            Match competitionEDF = new Match(new List<Team>() { teamE, teamD,teamF }, DateTime.Now, played);
+            //Create results.
+            Result resultDEF = new Result();
+            resultDEF.Add(teamD, 2);
+            resultDEF.Add(teamE, 3);
+            resultDEF.Add(teamF, 1);
+            Result resultED = new Result();
+            resultED.Add(teamE, 2);
+            resultED.Add(teamD, 1);
+            Result resultEDF = new Result();
+            resultEDF.Add(teamE, 1);
+            resultEDF.Add(teamD, 2);
+            resultEDF.Add(teamF, 3);
+            //Assign results to competitions.
+            competitiomDEF.Result = resultDEF;
+            competitionED.Result = resultED;
+            competitionEDF.Result = resultEDF;
+            return new List<Encounter>() { competitiomDEF, competitionED, competitionEDF };
         }
     }
 }
