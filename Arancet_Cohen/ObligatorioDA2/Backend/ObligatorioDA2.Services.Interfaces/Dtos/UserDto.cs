255a1ffb385174b483404c2bc190d74c32fd382d
Marcel Cohen <diegomarcel27@hotmail.com>
2018-11-02 16:37:44 -0300

    se crea un Dto que transferira los datos de usuario a registrar o modificar

---
+++ Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.Services.Interfaces/Dtos/UserDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObligatorioDA2.Services.Interfaces.Dtos
+{
+    public class UserDto
+    {
+        public string name;
+        public string surname;
+        public string username;
+        public string password;
+        public string email;
+        public bool isAdmin;
+    }
+}
