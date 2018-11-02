a74a11bc68a36b3615975f70b406e3d824bdd428
Marcel Cohen <diegomarcel27@hotmail.com>
2018-11-02 16:56:31 -0300

    se crea ModelIn para actualizacion de usuarios, no tiene campos requeridos, solo se asignan los que no son nulos

---
+++ Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.WebAPI/Models/UpdateUserModelIn.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ObligatorioDA2.WebAPI.Models
+{
+    public class UpdateUserModelIn
+    {
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+
+        public bool IsAdmin { get; set; }
+    }
+}
