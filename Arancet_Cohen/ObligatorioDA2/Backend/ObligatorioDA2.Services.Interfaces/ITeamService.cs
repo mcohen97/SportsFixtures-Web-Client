c2cd16f29a5360926509c9a1edd25d1b8c962aef
Marcel Cohen <diegomarcel27@hotmail.com>
2018-11-03 13:45:27 -0300

    se agrega metodo testeado a la interface

--- Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.Services.Interfaces/ITeamService.cs
+++ Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.Services.Interfaces/ITeamService.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Text;
 using ObligatorioDA2.BusinessLogic;
+using ObligatorioDA2.Services.Interfaces.Dtos;
 
 namespace ObligatorioDA2.Services.Interfaces
 {
     public interface ITeamService
     {
         Team GetTeam(int id);
+        Team AddTeam(TeamDto testDto);
     }
 }
