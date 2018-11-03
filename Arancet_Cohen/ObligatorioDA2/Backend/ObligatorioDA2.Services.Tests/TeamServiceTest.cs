d7de0a4623479ca3098ce74d4007bdb83dfd7583
Marcel Cohen <diegomarcel27@hotmail.com>
2018-11-03 13:14:56 -0300

    pruebas para AddTeam

--- Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.Services.Tests/TeamServiceTest.cs
+++ Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.Services.Tests/TeamServiceTest.cs
@@ -5,6 +5,7 @@ using ObligatorioDA2.BusinessLogic.Data.Exceptions;
 using ObligatorioDA2.Data.Repositories.Interfaces;
 using ObligatorioDA2.Services.Exceptions;
 using ObligatorioDA2.Services.Interfaces;
+using ObligatorioDA2.Services.Interfaces.Dtos;
 
 namespace ObligatorioDA2.Services.Tests
 {
@@ -18,14 +19,17 @@ namespace ObligatorioDA2.Services.Tests
         private Mock<IAuthenticationService> auth;
         private Sport testSport;
         private Team testTeam;
+        private TeamDto testDto;
 
         [TestInitialize]
         public void SetUp() {
             sports = new Mock<ISportRepository>();
             teams = new Mock<ITeamRepository>();
+            auth = new Mock<IAuthenticationService>();
             testService = new TeamService(sports.Object, teams.Object, auth.Object);
             testSport = new Sport("football", true);
             testTeam = new Team(1,"Real Madrid", "photo", testSport);
+            testDto = new TeamDto() { name = testTeam.Name, photo = testTeam.PhotoPath, sportName = testSport.Name };
         }
 
         [TestMethod]
@@ -50,5 +54,47 @@ namespace ObligatorioDA2.Services.Tests
             testService.GetTeam(1);
         }
 
+        [TestMethod]
+        public void AddTeamTest() {
+            auth.Setup(r => r.IsLoggedIn()).Returns(true);
+            auth.Setup(r => r.HasAdminPermissions()).Returns(true);
+            sports.Setup(r => r.Get(testSport.Name)).Returns(testSport);
+            teams.Setup(r => r.Add(testTeam)).Returns(testTeam);
+
+            Team added =testService.AddTeam(testDto);
+
+            auth.VerifyAll();
+            sports.VerifyAll();
+            teams.VerifyAll();
+            Assert.AreEqual(testTeam.Id, added.Id);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ServiceException))]
+        public void AddTeamNotExistentSportTest() {
+            sports.Setup(r => r.Get(It.IsAny<string>())).Throws(new SportNotFoundException());
+            teams.Setup(r => r.Add(testTeam)).Returns(testTeam);
+
+            Team added = testService.AddTeam(testDto);    
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ServiceException))]
+        public void AddTeamNotLoggedException() {
+            auth.Setup(r => r.IsLoggedIn()).Returns(false);
+            auth.Setup(r => r.HasAdminPermissions()).Returns(false);
+
+            Team added = testService.AddTeam(testDto);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ServiceException))]
+        public void AddTeamNotAuthorizedException()
+        {
+            auth.Setup(r => r.IsLoggedIn()).Returns(true);
+            auth.Setup(r => r.HasAdminPermissions()).Returns(false);
+
+            Team added = testService.AddTeam(testDto);
+        }
     }
 }
