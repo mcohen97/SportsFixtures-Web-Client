a5e27decd78d13418491772d4bc50bb9ebc0bd26
Marcel Cohen <diegomarcel27@hotmail.com>
2018-11-03 12:30:15 -0300

    se implementa metodo probado

--- Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.Services/TeamService.cs
+++ Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.Services/TeamService.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ObligatorioDA2.BusinessLogic;
+using ObligatorioDA2.BusinessLogic.Data.Exceptions;
 using ObligatorioDA2.Data.Repositories.Interfaces;
+using ObligatorioDA2.Services.Exceptions;
 using ObligatorioDA2.Services.Interfaces;
+using ObligatorioDA2.Services.Interfaces.Exceptions;
 
 namespace ObligatorioDA2.Services
 {
@@ -17,6 +21,36 @@ namespace ObligatorioDA2.Services
             sports = sportsRepo;
             teams = teamsRepo;
             authentication = authService;
+        }
+
+        public Team GetTeam(int id)
+        {
+            Team toReturn;
+            if (authentication.IsLoggedIn())
+            {
+                toReturn = TryGetTeam(id);
+            }
+            else {
+                throw new NotAuthenticatedException();
+            }
+            return toReturn;
+        }
+
+        private Team TryGetTeam(int id)
+        {
+            Team toReturn;
+            try
+            {
+                toReturn = teams.Get(id);
+            }
+            catch (TeamNotFoundException e)
+            {
+                throw new ServiceException(e.Message, ErrorType.ENTITY_NOT_FOUND);
+            }
+            catch (DataInaccessibleException e) {
+                throw new ServiceException(e.Message, ErrorType.DATA_INACCESSIBLE);
+            }
+            return toReturn;
         }
     }
 }
