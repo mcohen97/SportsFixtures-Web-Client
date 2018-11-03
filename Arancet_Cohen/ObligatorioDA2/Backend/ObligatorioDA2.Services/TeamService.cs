820ec33419726eef32457c9e5cce732d80cc716b
Marcel Cohen <diegomarcel27@hotmail.com>
2018-11-03 11:51:21 -0300

    se crea la clase TeamService testeada y su interface

---
+++ Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.Services/TeamService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ObligatorioDA2.Data.Repositories.Interfaces;
+using ObligatorioDA2.Services.Interfaces;
+
+namespace ObligatorioDA2.Services
+{
+    public class TeamService: ITeamService
+    {
+        private ISportRepository sports;
+        private ITeamRepository teams;
+        private IAuthenticationService authentication;
+
+        public TeamService(ISportRepository sportsRepo, ITeamRepository teamsRepo, IAuthenticationService authService)
+        {
+            sports = sportsRepo;
+            teams = teamsRepo;
+            authentication = authService;
+        }
+    }
+}
