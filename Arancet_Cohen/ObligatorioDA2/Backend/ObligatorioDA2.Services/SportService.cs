d1289f42301f73f7a5d3746702a06a1ae887ba12
Marcel Cohen <diegomarcel27@hotmail.com>
2018-11-04 18:41:31 -0300

    se crea el contrato del servicio referenciado en la prueba y la clase servicio que lo implementa

---
+++ Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.Services/SportService.cs
@@ -0,0 +1,19 @@
+﻿using ObligatorioDA2.Data.Repositories.Interfaces;
+using ObligatorioDA2.Services.Interfaces;
+
+namespace ObligatorioDA2.Services
+{
+    public class SportService : ISportService
+    {
+        private ISportRepository sports;
+        private ITeamRepository teams;
+        private IAuthenticationService authenticator;
+
+        public SportService(ISportRepository sportRepository, ITeamRepository teamRepository, IAuthenticationService authService)
+        {
+            sports = sportRepository;
+            teams = teamRepository;
+            authenticator = authService;
+        }
+    }
+}
