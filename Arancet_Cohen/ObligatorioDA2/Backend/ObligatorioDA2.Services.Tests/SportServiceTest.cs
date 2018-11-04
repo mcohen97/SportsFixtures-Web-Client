1c66421a11ddf9025fd61b6febab396f238ebd92
Marcel Cohen <diegomarcel27@hotmail.com>
2018-11-04 17:33:21 -0300

    se agrega clase de prueba para SportService

---
+++ Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.Services.Tests/SportServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using ObligatorioDA2.Data.Repositories.Interfaces;
+using ObligatorioDA2.Services.Interfaces;
+
+namespace ObligatorioDA2.Services.Tests
+{
+    public class SportServiceTest
+    {
+        private ISportService serviceToTest;
+        private Mock<ISportRepository> sportsStorage;
+        private Mock<ITeamRepository> teamsStorage;
+        private Mock<IAuthenticationService> authentication;
+
+        [TestInitialize]
+        public void SetUp() {
+            sportsStorage = new Mock<ISportRepository>();
+            teamsStorage = new Mock<ITeamRepository>();
+            serviceToTest = new SportService(sportsStorage.Object, teamsStorage.Object, authentication.Object);
+        }
+    }
+}
