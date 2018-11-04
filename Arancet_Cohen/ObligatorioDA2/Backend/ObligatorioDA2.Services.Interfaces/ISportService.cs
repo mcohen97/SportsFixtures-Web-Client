7e294cc80bbf3033823a2eec6dbdc0e99af3b59d
Marcel Cohen <diegomarcel27@hotmail.com>
2018-11-04 19:07:56 -0300

    se agrega el metodo probado al contrato del servicio

--- Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.Services.Interfaces/ISportService.cs
+++ Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.Services.Interfaces/ISportService.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ObligatorioDA2.BusinessLogic;
 
 namespace ObligatorioDA2.Services.Interfaces
 {
     public interface ISportService
     {
+        ICollection<Sport> GetAllSports();
     }
 }
