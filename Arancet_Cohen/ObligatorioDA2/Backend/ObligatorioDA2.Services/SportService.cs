2cea1849c0e0cc6c001c902dabd757d65e357a70
Marcel Cohen <diegomarcel27@hotmail.com>
2018-11-04 19:08:09 -0300

    se implementa metodo para que las pruebas pasen

--- Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.Services/SportService.cs
+++ Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.Services/SportService.cs
@@ -1,4 +1,8 @@
-﻿using ObligatorioDA2.Data.Repositories.Interfaces;
+﻿using System.Collections.Generic;
+using ObligatorioDA2.BusinessLogic;
+using ObligatorioDA2.BusinessLogic.Data.Exceptions;
+using ObligatorioDA2.Data.Repositories.Interfaces;
+using ObligatorioDA2.Services.Exceptions;
 using ObligatorioDA2.Services.Interfaces;
 
 namespace ObligatorioDA2.Services
@@ -14,6 +18,41 @@ namespace ObligatorioDA2.Services
             sports = sportRepository;
             teams = teamRepository;
             authenticator = authService;
+        }
+
+        public ICollection<Sport> GetAllSports()
+        {
+            Authenticate();
+            ICollection<Sport> allOfThem;
+            try
+            {
+                allOfThem = sports.GetAll();
+            }
+            catch (DataInaccessibleException e) {
+                throw new ServiceException(e.Message, ErrorType.DATA_INACCESSIBLE);
+            }
+            return allOfThem;
+        }
+
+        private void AuthenticateAdmin()
+        {
+            if (!authenticator.IsLoggedIn())
+            {
+                throw new NotAuthenticatedException();
+            }
+
+            if (!authenticator.HasAdminPermissions())
+            {
+                throw new NoPermissionsException();
+            }
+        }
+
+        private void Authenticate()
+        {
+            if (!authenticator.IsLoggedIn())
+            {
+                throw new NotAuthenticatedException();
+            }
         }
     }
 }
