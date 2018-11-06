905513a8cf9455c9b04cc36d711f1c9215cd4245
Marcel Cohen <diegomarcel27@hotmail.com>
2018-11-06 17:44:16 -0300

    se modifica la interfaz de IFixtureService para que solo maneje dtos

--- Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.Services.Interfaces/IFixtureService.cs
+++ Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.Services.Interfaces/IFixtureService.cs
@@ -2,15 +2,16 @@
 using System.Collections.Generic;
 using System.Text;
 using ObligatorioDA2.BusinessLogic;
+using ObligatorioDA2.Services.Interfaces.Dtos;
 
 namespace ObligatorioDA2.Services.Interfaces
 {
     public interface IFixtureService
     {
         IFixtureGenerator FixtureAlgorithm { get; set; }
-        ICollection<Encounter> AddFixture(ICollection<Team> teamsCollection);
-        ICollection<Encounter> AddFixture(ICollection<string> teamsNames, string sportName);
-        ICollection<Encounter> AddFixture(Sport sport);
+        ICollection<EncounterDto> AddFixture(ICollection<Team> teamsCollection);
+        ICollection<EncounterDto> AddFixture(ICollection<string> teamsNames, string sportName);
+        ICollection<EncounterDto> AddFixture(Sport sport);
         ICollection<Type> GetAlgorithms(string dllPath);
     }
 }
