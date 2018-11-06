d08765e7b48ed7f133cf7e5cee56965ce0f605e4
Marcel Cohen <diegomarcel27@hotmail.com>
2018-11-06 20:10:38 -0300

    se cambia el contrato de ISportService para que solo manipule dtos

--- Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.Services.Interfaces/ISportService.cs
+++ Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.Services.Interfaces/ISportService.cs
@@ -1,16 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using ObligatorioDA2.BusinessLogic;
+﻿using System.Collections.Generic;
 using ObligatorioDA2.Services.Interfaces.Dtos;
 
 namespace ObligatorioDA2.Services.Interfaces
 {
     public interface ISportService
     {
-        ICollection<Sport> GetAllSports();
-        Sport GetSport(string name);
-        Sport AddSport(SportDto testDto);
+        ICollection<SportDto> GetAllSports();
+        SportDto GetSport(string name);
+        SportDto AddSport(SportDto testDto);
         void DeleteSport(string name);
     }
 }
