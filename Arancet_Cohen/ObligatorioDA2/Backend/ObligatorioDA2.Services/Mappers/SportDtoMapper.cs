b913d17016b33ff23f1687a145fa2a4e32b20559
Marcel Cohen <diegomarcel27@hotmail.com>
2018-11-06 20:06:20 -0300

    se crea Mapper de Sport y SportDto

---
+++ Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.Services/Mappers/SportDtoMapper.cs
@@ -0,0 +1,19 @@
+﻿using ObligatorioDA2.BusinessLogic;
+using ObligatorioDA2.Services.Interfaces.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObligatorioDA2.Services.Mappers
+{
+    public class SportDtoMapper
+    {
+        public SportDto ToDto(Sport aSport) {
+            return new SportDto() { name = aSport.Name, isTwoTeams = aSport.IsTwoTeams };
+        }
+
+        public Sport ToSport(SportDto dto) {
+            return new Sport(dto.name, dto.isTwoTeams);
+        }
+    }
+}
