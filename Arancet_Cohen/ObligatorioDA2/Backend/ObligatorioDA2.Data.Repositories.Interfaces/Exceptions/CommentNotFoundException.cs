1033ce697dd8dbdfc6d5075550b626b04f402146
Marcel Cohen <diegomarcel27@hotmail.com>
2018-11-06 17:36:28 -0300

    se cambia la clase de la que hereda una excepcion

--- Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.Data.Repositories.Interfaces/Exceptions/CommentNotFoundException.cs
+++ Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.Data.Repositories.Interfaces/Exceptions/CommentNotFoundException.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace ObligatorioDA2.BusinessLogic.Data.Exceptions
 {
-    public class CommentNotFoundException:DataAccessException
+    public class CommentNotFoundException:EntityNotFoundException
     {
         public CommentNotFoundException():base("Couldn't find comment") {
 
