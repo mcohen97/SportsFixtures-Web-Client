51ae57f3dcb4a2ea25d8214d3fffb5f054dd50d7
Marcel Cohen <diegomarcel27@hotmail.com>
2018-11-06 21:34:29 -0300

    se renombra archivo

--- Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.Services.Interfaces/IAuthenticationService.cs
+++ Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.Services.Interfaces/IAuthenticationService.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using ObligatorioDA2.BusinessLogic;
+﻿
+using ObligatorioDA2.Services.Interfaces.Dtos;
 
 namespace ObligatorioDA2.Services.Interfaces
 {
     public interface IAuthenticationService
     {
-        User Login(string username, string password);
+        UserDto Login(string username, string password);
         void SetSession(string userName);
         bool HasAdminPermissions();
 
