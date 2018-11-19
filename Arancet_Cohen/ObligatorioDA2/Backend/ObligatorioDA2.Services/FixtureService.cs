ed6a08fa950a6e5f7db3c348897a37f98dfc7592
Marcel Cohen <diegomarcel27@hotmail.com>
2018-11-19 16:46:07 -0300

    se cubre caso superborde

--- Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.Services/FixtureService.cs
+++ Arancet_Cohen/ObligatorioDA2/Backend/ObligatorioDA2.Services/FixtureService.cs
@@ -146,9 +146,9 @@ namespace ObligatorioDA2.Services
                 {
                     continue;  // Can't load as .NET assembly, so ignore
                 }
-                interestingTypes =
-                    types.Where(t => t.IsClass &&
-                                     t.GetInterfaces().Contains(typeof(IFixtureGenerator)));
+                interestingTypes= interestingTypes.Concat(
+                                    types.Where(t => t.IsClass &&
+                                    t.GetInterfaces().Contains(typeof(IFixtureGenerator))));
             }
             return interestingTypes.ToList();
         }
@@ -174,9 +174,15 @@ namespace ObligatorioDA2.Services
             int daysBetweenRounds = dto.daysBetweenRounds == 0 ? 7 : dto.daysBetweenRounds;
 
             Type algortihmType = GetAlgorithmType(algorithmsPath, dto.fixtureName);
-            object fromDll = Activator.CreateInstance(algortihmType, new object[] { dto.initialDate, roundLength, daysBetweenRounds });
-            IFixtureGenerator algorithm = fromDll as IFixtureGenerator;
-            return algorithm;
+            try
+            {
+                object fromDll = Activator.CreateInstance(algortihmType, new object[] { dto.initialDate, roundLength, daysBetweenRounds });
+                IFixtureGenerator algorithm = fromDll as IFixtureGenerator;
+                return algorithm;
+            }
+            catch (MissingMemberException e) {
+                throw new ServiceException(e.Message, ErrorType.INVALID_DATA);
+            }
         }
 
         private Type GetAlgorithmType(string algorithmsPath, string fixtureName)
